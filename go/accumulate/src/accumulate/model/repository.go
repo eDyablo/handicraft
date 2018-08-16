18471c5e6fea778457874afea6017c996f368ec8
Eduard Iablonskyi <eiablonskyi@lohika.com>
2018-08-16 14:59:52 +0300

    format code

--- go/accumulate/src/accumulate/model/repository.go
+++ go/accumulate/src/accumulate/model/repository.go
@@ -1,45 +1,45 @@
 package model
 
 import (
-	"github.com/satori/go.uuid"
+  "github.com/satori/go.uuid"
 )
 
 // Repository stores accumulators
 type Repository struct {
-	Accumulators map[string]uint64
+  Accumulators map[string]uint64
 }
 
 // NewRepository creates new empty repository
 func NewRepository() Repository {
-	return Repository{
-		Accumulators: make(map[string]uint64),
-	}
+  return Repository{
+    Accumulators: make(map[string]uint64),
+  }
 }
 
 // NewAccumulator creates new accumulator and adds it to the repository
 func (repository Repository) NewAccumulator() Accumulator {
-	accumulator := Accumulator{
-		ID:    uuid.Must(uuid.NewV4()).String(),
-		Value: 0,
-	}
-	repository.Accumulators[accumulator.ID] = accumulator.Value
-	return accumulator
+  accumulator := Accumulator{
+    ID:    uuid.Must(uuid.NewV4()).String(),
+    Value: 0,
+  }
+  repository.Accumulators[accumulator.ID] = accumulator.Value
+  return accumulator
 }
 
 // GetAccumulator returns accumulator with specified id
 func (repository Repository) GetAccumulator(id string) Accumulator {
-	return Accumulator{
-		ID:    id,
-		Value: repository.Accumulators[id],
-	}
+  return Accumulator{
+    ID:    id,
+    Value: repository.Accumulators[id],
+  }
 }
 
 // UpdateAccumulator updates specified accumulator
 func (repository Repository) UpdateAccumulator(accumulator Accumulator) {
-	repository.Accumulators[accumulator.ID] = accumulator.Value
+  repository.Accumulators[accumulator.ID] = accumulator.Value
 }
 
 // DeleteAccumulator deletes specified accumulator
 func (repository Repository) DeleteAccumulator(accumulator Accumulator) {
-	delete(repository.Accumulators, accumulator.ID)
+  delete(repository.Accumulators, accumulator.ID)
 }
