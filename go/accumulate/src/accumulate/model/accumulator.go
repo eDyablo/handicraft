df2faba3a284524eb7b10a7f1331eb216e9ced20
Eduard Iablonskyi <eiablonskyi@lohika.com>
2018-08-16 14:58:58 +0300

    format code

--- go/accumulate/src/accumulate/model/accumulator.go
+++ go/accumulate/src/accumulate/model/accumulator.go
@@ -2,6 +2,6 @@ package model
 
 // Accumulator represents accumulator
 type Accumulator struct {
-	ID    string `json:"id"`
-	Value uint64 `json:"value"`
+  ID    string `json:"id"`
+  Value uint64 `json:"value"`
 }
