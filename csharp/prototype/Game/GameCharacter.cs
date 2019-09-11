6e30b7863357509245006547748d804d59f8c8c2
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-11 15:56:25 +0300

    add position tests

--- csharp/prototype/Game/GameCharacter.cs
+++ csharp/prototype/Game/GameCharacter.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace prototype.Game
 {
   public class GameCharacter : GameObject {
     public string Name { get; set; }
+    public float HitPoints { get; set; }
 
     public GameCharacter() {
       Position = new GameMapPosition();
@@ -10,8 +13,13 @@ namespace prototype.Game
     public override GameObject Clone() {
       return new GameCharacter {
         Name = Name,
-        Position = new GameMapPosition(),
+        HitPoints = HitPoints,
+        Position = GameMapPosition.CopyFrom(Position),
       };
+    }
+
+    public void MoveTo(GameMapPosition position) {
+      Position = GameMapPosition.CopyFrom(position);
     }
   }
 }
