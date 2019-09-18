ac24dfe18cec4a9e7e4ba67f5de9538e1db5d98c
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-18 16:48:07 +0300

    mail composer supports header and footer

--- js/builder/mail-composer.js
+++ js/builder/mail-composer.js
@@ -6,12 +6,16 @@ module.exports = class MailComposer {
     this.to = options.to;
     this.from = options.from;
     this.body = options.body;
+    this.header = options.header;
+    this.footer = options.footer;
   }
 
   compose() {
+    this.builder.buildHeading(this.header);
     this.builder.buildParagraph('Dear ' + this.to + ',');
     this.builder.buildParagraph(this.body);
     this.builder.buildParagraph('Sincerely yours,');
     this.builder.buildParagraph(this.from);
+    this.builder.buildHeading(this.footer);
   }
 }
