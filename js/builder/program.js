a51f0dc8f4c7ab174751bf6f3227984e1e75b376
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-18 15:14:30 +0300

    use html builder

--- js/builder/program.js
+++ js/builder/program.js
@@ -1,6 +1,9 @@
-const MarkdownReader = require('./markdown-reader')
-const DomBuilder = require('./dom-builder')
+'use strict';
+
+const MarkdownReader = require('./markdown-reader');
+const DomBuilder = require('./dom-builder');
 const TextBuilder = require('./text-builder');
+const HtmlBuilder = require('./html-builder');
 
 const markdown = `
 # greeting
@@ -11,20 +14,27 @@ i am a js code
 # farewell
 
 good bye
-`
+`;
 
 function toDom(markdown) {
   const builder = new DomBuilder();
   const reader = new MarkdownReader(builder);
-  reader.read(markdown)
+  reader.read(markdown);
   return builder.getDom().body.innerHTML;
 }
 
 function toText(markdown) {
   const builder = new TextBuilder();
   const reader = new MarkdownReader(builder);
-  reader.read(markdown)
+  reader.read(markdown);
   return builder.getText();
+}
+
+function toHtml(markdown) {
+  const builder = new HtmlBuilder();
+  const reader = new MarkdownReader(builder);
+  reader.read(markdown);
+  return builder.getHtml();
 }
 
 console.log('DOM\n');
@@ -32,3 +42,6 @@ console.log(toDom(markdown));
 
 console.log('\ntext\n');
 console.log(toText(markdown));
+
+console.log('\nHTML\n');
+console.log(toHtml(markdown));
