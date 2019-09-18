3819ceab12dd456271e9bd424abc5e46a59ccf28
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-18 16:54:14 +0300

    compose mail as html

--- js/builder/compose-mail.js
+++ js/builder/compose-mail.js
@@ -2,15 +2,31 @@
 
 const MailComposer = require('./mail-composer')
 const TextBuilder = require('./text-builder')
+const HtmlBuilder = require('./html-builder')
 
-const builder = new TextBuilder();
-const composer = new MailComposer(builder, {
+const mail = {
   from: 'Ed',
   to: 'Santa',
   body: 'Would you kindly do me a favor and ...',
   header: 'ACME Inc.',
-});
-composer.compose();
-const mail = builder.getText();
+};
 
-console.log(mail);
+function textMail(mail) {
+  const builder = new TextBuilder();
+  const composer = new MailComposer(builder, mail);
+  composer.compose();
+  return builder.getText();
+}
+
+function htmlMail(mail) {
+  const builder = new HtmlBuilder();
+  const composer = new MailComposer(builder, mail);
+  composer.compose();
+  return builder.getHtml();
+}
+
+console.log('mail as text\n')
+console.log(textMail(mail));
+
+console.log('\nmail as HTML\n')
+console.log(htmlMail(mail));
