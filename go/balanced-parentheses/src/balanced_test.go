d18b3e823d0468d45b1f4ae6528e8b6b790cf4a9
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-22 18:50:30 +0300

    [red]  signle opening parenthesis is non balanced

--- go/balanced-parentheses/src/balanced_test.go
+++ go/balanced-parentheses/src/balanced_test.go
@@ -3,6 +3,11 @@ package src
 import "testing"
 
 func Test_empty_text_is_balanced(test *testing.T) {
-  if balanced("") { return }
+  if balanced("") == true { return }
+  test.Error()
+}
+
+func Test_signle_opening_parenthesis_is_non_balanced(test *testing.T) {
+  if balanced("(") == false { return }
   test.Error()
 }
