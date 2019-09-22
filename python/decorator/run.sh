178a01b0b479d50c2e073037735a497696234537
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-09-22 12:38:39 +0300

    improve run script

--- python/decorator/run.sh
+++ python/decorator/run.sh
@@ -3,4 +3,17 @@
 set -o errexit
 set -o pipefail
 
-test_result=$(pytest) && python src/program.py || echo "${test_result}"
+venv_dir=".venv"
+
+test() {
+  . "${venv_dir}/bin/activate"
+  test_result=$(pytest) && python "src/program.py" || echo "${test_result}"
+}
+
+boot_strap() {
+  virtualenv .venv
+  . "${venv_dir}/bin/activate"
+  pip install -r "requirements"
+}
+
+[ -d "${venv_dir}" ] && test || ( boot_strap && test )
