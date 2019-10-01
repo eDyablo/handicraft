3a27707c8e1ca689e9ead7211d1e74bee33bc315
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-01 11:18:40 +0300

    extract checkout method

--- groovy/bridge/src/main/groovy/IntegrationJob.groovy
+++ groovy/bridge/src/main/groovy/IntegrationJob.groovy
@@ -1,6 +1,10 @@
 class IntegrationJob extends PipelineJob {
   void run() {
     echo('integrate')
+    checkout()
+  }
+
+  void checkout() {
     final CheckoutStep step = new CheckoutStep(impl: impl)
     step.doStep()
   }
