76950a3f5c7bae272eef9ede54066efe983a6711
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-10 16:25:24 +0300

    convert indentation

--- go/command/command/append_test.go
+++ go/command/command/append_test.go
@@ -1,8 +1,8 @@
 package command
 
 import (
-	"strings"
-	"testing"
+  "strings"
+  "testing"
 )
 
 type Test struct {
@@ -11,59 +11,59 @@ type Test struct {
 
 func Test_append_adds_one_empty_line_when_input_is_empty(test *testing.T) {
   // Arrange
-	reader := strings.NewReader("")
-	text := []string {}
-	cntx := TestContext {}
-	cmd := Append { Reader: reader, Text: &text }
+  reader := strings.NewReader("")
+  text := []string {}
+  cntx := TestContext {}
+  cmd := Append { Reader: reader, Text: &text }
   // Act
-	cmd.Execute(&cntx)
-	cntx.command.Execute(&cntx)
+  cmd.Execute(&cntx)
+  cntx.command.Execute(&cntx)
   // Assert
-	if len(text) == 1 { return }
-	test.Error(text)
+  if len(text) == 1 { return }
+  test.Error(text)
 }
 
 func Test_append_does_not_add_text_when_input_is_dot(test *testing.T) {
   // Arrange
-	reader := strings.NewReader(".")
-	text := []string {}
-	cntx := TestContext {}
-	cmd := Append { Reader: reader, Text: &text }
+  reader := strings.NewReader(".")
+  text := []string {}
+  cntx := TestContext {}
+  cmd := Append { Reader: reader, Text: &text }
   // Act
-	cmd.Execute(&cntx)
-	cntx.command.Execute(&cntx)
+  cmd.Execute(&cntx)
+  cntx.command.Execute(&cntx)
   // Assert
-	if len(text) == 0 { return }
-	test.Error(text)
+  if len(text) == 0 { return }
+  test.Error(text)
 }
 
 func Test_append_text_adds_lines_to_the_end_of_the_text(test *testing.T) {
   // Arrange
-	cntx := TestContext {}
-	text := []string { "one", "two" }
-	addition := []string { "three", "four" }
-	cmd := AppendText { Lines: addition, Text: &text }
+  cntx := TestContext {}
+  text := []string { "one", "two" }
+  addition := []string { "three", "four" }
+  cmd := AppendText { Lines: addition, Text: &text }
   // Act
-	cmd.Execute(&cntx)
+  cmd.Execute(&cntx)
   // Assert
-	if len(text) == 4 &&
-		text[0] == "one" &&
-		text[3] == "four" { return }
-	test.Error(text)
+  if len(text) == 4 &&
+    text[0] == "one" &&
+    text[3] == "four" { return }
+  test.Error(text)
 }
 
 func Test_append_text_enqueue_undo_command(test *testing.T) {
-	// Arrange
-	cntx := TestContext {}
-	text := []string { "one", "two" }
-	addition := []string { "three", "four" }
-	cmd := AppendText { Lines: addition, Text: &text }
-	// Act
-	cmd.Execute(&cntx)
-	cntx.undoCommand.Execute(NullContext())
-	// Assert
-	if len(text) == 2 &&
-		text[0] == "one" &&
-		text[1] == "two" { return }
-	test.Error(text)
+  // Arrange
+  cntx := TestContext {}
+  text := []string { "one", "two" }
+  addition := []string { "three", "four" }
+  cmd := AppendText { Lines: addition, Text: &text }
+  // Act
+  cmd.Execute(&cntx)
+  cntx.undoCommand.Execute(NullContext())
+  // Assert
+  if len(text) == 2 &&
+    text[0] == "one" &&
+    text[1] == "two" { return }
+  test.Error(text)
 }
