0a7160e6ed15e7b8a79fd27f4eeda38f67f54f92
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-19 19:47:37 +0300

    add lightbulbs to turnstyle

--- csharp/state/turnstyle/Machine.cs
+++ csharp/state/turnstyle/Machine.cs
@@ -4,16 +4,25 @@ namespace Turnstyle
   {
     public State State;
 
+    public bool GreenLightbulb { get; private set; }
+
+    public bool RedLightbulb { get; private set; }
+
     public virtual void Unlock()
     {
+      GreenLightbulb = true;
+      RedLightbulb = false;
     }
 
     public virtual void Alarm()
     {
+      RedLightbulb = true;
     }
 
     public virtual void Lock()
     {
+      RedLightbulb = true;
+      GreenLightbulb = false;
     }
 
     public void Coin() => State.Coin(this);
