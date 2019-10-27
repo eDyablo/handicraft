5ae769a9651c2b003e5f5a4fe68f7737cde168c7
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-27 10:04:19 +0200

    improve spin commands

--- java/marsrover/src/marsrover/Mission.java
+++ java/marsrover/src/marsrover/Mission.java
@@ -1,20 +1,66 @@
 package marsrover;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mission {
   public int xRange;
   public int yRange;
+  public List<String> result = new ArrayList<String>();
+  private String[] rover;
 
-	public String[] explore(String[] plan) {
+	public void explore(String[] plan) {
     if (plan.length > 0) {
-      String[] range = plan[0].split(" ");
-      xRange = Integer.parseInt(range[0]);
-      yRange = Integer.parseInt(range[1]);
+      determineRange(plan);
       if (plan.length > 1) {
-        return new String[] {
-          plan[1],
-        };
+        rover = plan[1].split(" ");
+        if (plan.length > 2) {
+          for (Character command : plan[2].toCharArray()) {
+            commandRover(command);
+          }
+        }
+        result.add(String.join(" ", rover));
       }
     }
-    return new String[]{};
-	}
+  }
+  
+  private void determineRange(String[] plan) {
+    String[] range = plan[0].split(" ");
+    xRange = Integer.parseInt(range[0]);
+    yRange = Integer.parseInt(range[1]);
+  }
+
+  private void commandRover(Character command) {
+    if (command == 'L') {
+      spinRoverLeft();
+    }
+    if (command == 'R') {
+      spinRoverRight();
+    }
+    if (command == 'M') {
+      moveRover();
+    }
+  }
+
+  private void spinRoverLeft() {
+    if (rover[2].equals("N")) {
+      rover[2] = "E";
+    } else if (rover[2].equals("E")) {
+      rover[2] = "S";
+    }
+  }
+
+  private void spinRoverRight() {
+    if (rover[2].equals("N")) {
+      rover[2] = "W";
+    } else if (rover[2].equals("W")) {
+      rover[2] = "S";
+    }
+  }
+
+  private void moveRover() {
+    int y = Integer.parseInt(rover[1]);
+    y++;
+    rover[1] = String.valueOf(y);
+  }
 }
--- java/marsrover/test/marsrover/MissionTest.java
+++ java/marsrover/test/marsrover/MissionTest.java
@@ -8,8 +8,8 @@ public class MissionTest {
   Mission mission = new Mission();
 
   @Test public void exploration_with_no_plan_givest_no_result() {
-    String[] result = mission.explore(new String[]{});
-    assertThat(result, is(emptyArray()));
+    mission.explore(new String[]{});
+    assertThat(mission.result, is(empty()));
   }
 
   @Test public void exploration_plan_sets_mission_range() {
@@ -21,10 +21,64 @@ public class MissionTest {
   }
 
   @Test public void exploration_with_one_rover_lands_the_rover_to_coordinates_and_direction_from_the_plan() {
-    String[] result = mission.explore(new String[]{
+    mission.explore(new String[]{
       "1 1",
       "1 1 N",
     });
-    assertThat(result, is(array(equalTo("1 1 N"))));
+    assertThat(mission.result, contains("1 1 N"));
+  }
+
+  @Test public void exloration_plan_with_one_L_command_spins_north_oriented_rover_to_east() {
+    mission.explore(new String[] {
+      "1 1",
+      "1 1 N",
+      "L"
+    });
+    assertThat(mission.result, contains("1 1 E"));
+  }
+
+  @Test public void exloration_plan_with_one_R_command_spins_north_oriented_rover_to_west() {
+    mission.explore(new String[] {
+      "1 1",
+      "1 1 N",
+      "R"
+    });
+    assertThat(mission.result, contains("1 1 W"));
+  }
+
+  @Test public void exloration_plan_with_one_M_command_moves_north_oriented_rover_one_step_towards_north() {
+    mission.explore(new String[] {
+      "1 1",
+      "0 0 N",
+      "M"
+    });
+    assertThat(mission.result, contains("0 1 N"));
+  }
+
+  @Test public void exloration_plan_with_two_M_commands_moves_north_oriented_rover_two_steps_towards_north() {
+    mission.explore(new String[] {
+      "2 2",
+      "0 0 N",
+      "MM"
+    });
+    assertThat(mission.result, contains("0 2 N"));
+  }
+
+  @Test public void exloration_plan_with_two_L_commands_spins_north_oriented_rover_to_south() {
+    mission.explore(new String[] {
+      "1 1",
+      "0 0 N",
+      "LL"
+    });
+    assertThat(mission.result, contains("0 0 S"));
+  }
+
+  @Test public void exloration_plan_with_two_R_commands_spins_north_oriented_rover_to_south() {
+    mission.explore(new String[] {
+      "1 1",
+      "0 0 N",
+      "RR"
+    });
+    assertThat(mission.result, contains("0 0 S"));
   }
 }
