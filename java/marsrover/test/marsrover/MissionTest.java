58757040e9909c7793146ddf66c286388383b87a
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-27 05:08:40 +0200

    initial test

---
+++ java/marsrover/test/marsrover/MissionTest.java
@@ -0,0 +1,11 @@
+package marsrover;
+
+import org.junit.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class MissionTest {
+  @Test public void rename() {
+    assertThat(1, is(1));
+  }
+}
