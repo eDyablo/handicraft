953e393d973553cb12b0ccefc7f63e3466a71abf
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-27 12:27:06 +0100

    first real tests

--- go/marsrover/mission.go
+++ go/marsrover/mission.go
@@ -1,7 +1,32 @@
 package marsrover
 
-// Mission represents exploration mission
-type Mission struct {
-  Result []string
+import (
+  "strings"
+  "strconv"
+)
+
+// Range represents exploration mission range
+type Range struct {
+  X int
+  Y int
 }
 
+// Mission represents exploration mission
+type Mission struct {
+  Plan   []string
+  Result []string
+  Range  Range
+}
+
+// Explore does exploration
+func (mission *Mission) Explore() {
+  if len(mission.Plan) > 0 {
+    items := strings.SplitN(mission.Plan[0], " ", 2)
+    x,_ := strconv.Atoi(items[0])
+    y,_ := strconv.Atoi(items[1])
+    mission.Range = Range { X: x, Y: y }
+    if len(mission.Plan) > 1 {
+      mission.Result = append(mission.Result, mission.Plan[1])
+    }
+  }
+}
--- go/marsrover/mission_test.go
+++ go/marsrover/mission_test.go
@@ -4,6 +4,28 @@ import "testing"
 
 func Test_exploration_without_plan_has_no_results(test *testing.T) {
   mission := Mission {}
+  mission.Explore()
   if (len(mission.Result) == 0) { return }
-  test.Error(mission.Result)
+  test.Error("Result is", mission.Result)
+}
+
+func Test_exploration_plan_sets_mission_range(test *testing.T) {
+  mission := Mission {
+    Plan: []string { "1 2" },
+  }
+  mission.Explore()
+  if (mission.Range.X == 1 && mission.Range.Y == 2) { return }
+  test.Error("Range is", mission.Range)
+}
+
+func Test_exploration_plan_for_one_rover_lands_the_rover_to_coordinates_with_direction_from_the_plan(test *testing.T) {
+  mission := Mission {
+    Plan: []string {
+      "1 1",
+      "1 2 N",
+    },
+  }
+  mission.Explore()
+  if (mission.Result[0] == "1 2 N") { return }
+  test.Error("Result is", mission.Result)
 }
