83e35380a92f377a32f051f76592a1ad414db5de
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-28 13:02:03 +0100

    convert arabic 10 to roman

--- groovy/roman-numerals/src/main/Numeral.groovy
+++ groovy/roman-numerals/src/main/Numeral.groovy
@@ -3,6 +3,10 @@ package numeral
 class Numeral {
   static String toRoman(int arabic) {
     final def roman = new StringBuilder()
+    while (arabic >= 10) {
+      roman << 'X'
+      arabic -= 10
+    }
     while (arabic >= 1) {
       roman << 'I'
       arabic -= 1
--- groovy/roman-numerals/src/test/NumeralTest.groovy
+++ groovy/roman-numerals/src/test/NumeralTest.groovy
@@ -7,9 +7,10 @@ import static org.hamcrest.Matchers.*;
 class NumeralTest {
   @Test void converts_arabic_numeral_to_roman_numeral() {
     final def tests = [
-      [1, 'I'],
-      [2, 'II'],
-      [3, 'III'],
+      [ 1, 'I'],
+      [ 2, 'II'],
+      [ 3, 'III'],
+      [10, 'X'],
     ]
     tests.each { arabic, roman ->
       assertThat("\n     For: ${ arabic }",
