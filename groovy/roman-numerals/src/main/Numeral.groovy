2133c00bc9ba599b9bb0fea7907d882df8856e05
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-28 13:00:53 +0100

    convert arabic 1,2 and 3 to roman

--- groovy/roman-numerals/src/main/Numeral.groovy
+++ groovy/roman-numerals/src/main/Numeral.groovy
@@ -2,6 +2,11 @@ package numeral
 
 class Numeral {
   static String toRoman(int arabic) {
-    return 'I'
+    final def roman = new StringBuilder()
+    while (arabic >= 1) {
+      roman << 'I'
+      arabic -= 1
+    }
+    return roman
   }
 }
--- groovy/roman-numerals/src/test/NumeralTest.groovy
+++ groovy/roman-numerals/src/test/NumeralTest.groovy
@@ -5,7 +5,15 @@ import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 class NumeralTest {
-  @Test void converts_arabic_1_to_roman_I() {
-    assertThat(Numeral.toRoman(1), is(equalTo('I')))
+  @Test void converts_arabic_numeral_to_roman_numeral() {
+    final def tests = [
+      [1, 'I'],
+      [2, 'II'],
+      [3, 'III'],
+    ]
+    tests.each { arabic, roman ->
+      assertThat("\n     For: ${ arabic }",
+        Numeral.toRoman(arabic), is(equalTo(roman)))
+    }
   }
 }
