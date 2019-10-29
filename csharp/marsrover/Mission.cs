f0c5fd152746037282062ea75e2af13a3dc5a3ca
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-10-29 09:27:20 +0100

    move range to distinct module

--- csharp/marsrover/Mission.cs
+++ csharp/marsrover/Mission.cs
@@ -8,7 +8,7 @@ namespace Marsrover
 
     public string[] Plan { private get; set; }
 
-    public RangeType Range { get; private set; }
+    public Range Range { get; private set; }
 
     public IEnumerable<string> Result { get => result; }
 
@@ -23,12 +23,6 @@ namespace Marsrover
     public const char RIGHT = 'R';
 
     public const char MOVE = 'M';
-
-    public struct RangeType
-    {
-      public int X { get; set; }
-      public int Y { get; set; }
-    }
 
     public void Explore()
     {
@@ -50,7 +44,7 @@ namespace Marsrover
     private void ReadRange()
     {
       var fields = Plan[0].Split(" ", 2);
-      Range = new RangeType
+      Range = new Range
       {
         X = int.Parse(fields[0]),
         Y = int.Parse(fields[1]),
---
+++ csharp/marsrover/Range.cs
@@ -0,0 +1,8 @@
+namespace Marsrover
+{
+  public struct Range
+  {
+    public int X { get; set; }
+    public int Y { get; set; }
+  }
+}
