52227bdf5a78d065b0bd5b94f6053e7fce6e9f2f
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-11-18 02:35:19 +0300

    adding first item to an empty list put it into first position

--- groovy/recently-used-list/src/main/RecentlyUsedList.groovy
+++ groovy/recently-used-list/src/main/RecentlyUsedList.groovy
@@ -1,5 +1,15 @@
 class RecentlyUsedList {
+  String item
+
   int getSize() {
     0
+  }
+
+  void add(String item) {
+    this.item = item
+  }
+
+  String getAt(int index) {
+    this.item
   }
 }
--- groovy/recently-used-list/src/test/RecentlyUsedListTest.groovy
+++ groovy/recently-used-list/src/test/RecentlyUsedListTest.groovy
@@ -7,4 +7,10 @@ class RecentlyUsedListTest {
     final list = new RecentlyUsedList()
     assertThat(list.size, is(equalTo(0)))
   }
+
+  @Test void adding_first_item_to_emty_list_places_it_to_first_position() {
+    final list = new RecentlyUsedList()
+    list.add('first')
+    assertThat(list.getAt(0), is(equalTo('first')))
+  }
 }
