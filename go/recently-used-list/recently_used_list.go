35d899e3a974b71d3d4d17d1b8529e3822d04b77
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-11-17 22:02:59 +0300

    rename variables

--- go/recently-used-list/recently_used_list.go
+++ go/recently-used-list/recently_used_list.go
@@ -3,27 +3,27 @@ package rul
 // RecentlyUsedList holds strings uniquely
 // in Last-In-First-Out order
 type RecentlyUsedList struct {
-  item []string
+  items []string
 }
 
 // Size returns number of items in the list
 func (list *RecentlyUsedList) Size() uint {
-  return uint(len(list.item))
+  return uint(len(list.items))
 }
 
 // Add adds an item to the list
 func (list *RecentlyUsedList) Add(item string) {
-  found := len(list.item)
-  for i, ci := range list.item {
-    if ci == item {
-      found = i
+  foundAt := len(list.items)
+  for index, listItem := range list.items {
+    if item == listItem {
+      foundAt = index
       break
     }
   }
-  list.item = append([]string {item}, list.item[0:found]...)
+  list.items = append([]string {item}, list.items[0:foundAt]...)
 }
 
 // Get returns an item by its index in the list
 func (list *RecentlyUsedList) Get(index uint) string {
-  return list.item[index]
+  return list.items[index]
 }
