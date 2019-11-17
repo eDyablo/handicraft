a3d924481e80600905eec13dc57b6173cafcc4ec
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-11-17 23:24:26 +0300

    tidy up tests

--- go/recently-used-list/recently_used_list_test.go
+++ go/recently-used-list/recently_used_list_test.go
@@ -3,46 +3,54 @@ package rul
 import "testing"
 
 func Test_an_item_added_to_empty_list_is_first(test *testing.T) {
-  // Arrange
-  list := RecentlyUsedList {}
-  // Act
-  list.Add("item")
-  // Assert
-  if list.Get(0) == "item" { return }
-  test.Error(list)
+	// Arrange
+	list := RecentlyUsedList{}
+	// Act
+	list.Add("item")
+	// Assert
+	if list.Get(0) == "item" {
+		return
+	}
+	test.Error(list)
 }
 
 func Test_two_different_items_added_to_empty_list_can_be_get_in_reverse_order(test *testing.T) {
-  // Arrange
-  list := RecentlyUsedList {}
-  // Act
-  list.Add("first")
-  list.Add("second")
-  // Assert
-  if list.Get(0) == "second" &&
-    list.Get(1) == "first" { return }
-  test.Error(list)
+	// Arrange
+	list := RecentlyUsedList{}
+	// Act
+	list.Add("first")
+	list.Add("second")
+	// Assert
+	if list.Get(0) == "second" &&
+		list.Get(1) == "first" {
+		return
+	}
+	test.Error(list)
 }
 
 func Test_does_not_add_the_same_item_twice_to_empty_list(test *testing.T) {
-  // Arrange
-  list := RecentlyUsedList {}
-  // Act
-  list.Add("the same")
-  list.Add("the same")
-  // Assert
-  if list.Size() == 1 { return }
-  test.Error(list)
+	// Arrange
+	list := RecentlyUsedList{}
+	// Act
+	list.Add("the same")
+	list.Add("the same")
+	// Assert
+	if list.Size() == 1 {
+		return
+	}
+	test.Error(list)
 }
 
 func Test_does_not_add_the_same_item_twice_to_no_empty_list(test *testing.T) {
-  // Arrange
-  list := RecentlyUsedList {}
-  list.Add("first")
-  // Act
-  list.Add("second")
-  list.Add("second")
-  // Assert
-  if list.Size() == 2 { return }
-  test.Error(list)
+	// Arrange
+	list := RecentlyUsedList{}
+	list.Add("first")
+	// Act
+	list.Add("second")
+	list.Add("second")
+	// Assert
+	if list.Size() == 2 {
+		return
+	}
+	test.Error(list)
 }
