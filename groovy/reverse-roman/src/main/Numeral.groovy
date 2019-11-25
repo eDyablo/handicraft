56cfccfeb4d692ec0c213e6f21f56fc14f712223
Ed <34471322+eduardi-earnin@users.noreply.github.com>
2019-11-25 11:16:01 -0800

    Groovy reverse roman (#31)

---
+++ groovy/reverse-roman/src/main/Numeral.groovy
@@ -0,0 +1,26 @@
+package numeral
+
+class Numeral {
+  static int toArabic(String roman) {
+    final digits = [
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    ]
+    int arabic
+    int delta
+    roman.each { symbol ->
+      final int newDelta = digits[symbol] ?: 0
+      arabic += newDelta
+      if (newDelta > delta) {
+        arabic -= 2 * delta
+      }
+      delta = newDelta
+    }
+    return arabic
+  }
+}
---
+++ groovy/reverse-roman/src/test/NumeralTest.groovy
@@ -0,0 +1,33 @@
+package numeral
+
+import org.junit.*
+import static org.junit.Assert.*
+import static org.hamcrest.Matchers.*
+
+class NumeralTest {
+  @Test void roman_to_arabic() {
+    final tests = [
+      [      '', 0],
+      [     'I', 1],
+      [    'II', 2],
+      [     'V', 5],
+      [     'X', 10],
+      [     'L', 50],
+      [    'IV', 4],
+      [    'IX', 9],
+      [    'XL', 40],
+      [     'C', 100],
+      [     'D', 500],
+      [     'M', 1000],
+      [ 'MCMXC', 1990],
+      ['MMVIII', 2008],
+      [  'XCIX', 99],
+      [ 'XLVII', 47],
+      [     ' ', 0],
+    ]
+    tests.each { roman, arabic ->
+      assertThat("\n     For: ${ roman }",
+        Numeral.toArabic(roman), is(equalTo(arabic)))
+    }
+  }
+}
