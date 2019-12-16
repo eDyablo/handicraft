fcd33371551496f874b8708095ce194849b826cf
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-12-16 15:05:57 +0200

    docs for prisoner

--- go/prisoners-dilemma/prisoner.go
+++ go/prisoners-dilemma/prisoner.go
@@ -1,18 +1,22 @@
 package dilemma
 
+// Prisoner represents a prisoner
 type Prisoner struct {
   Name string
   Strategy PrisonerStrategy
 }
 
+// RemainSilent responds silent testimony
 func (prisoner *Prisoner) RemainSilent(accomplice *Prisoner) Testimony {
   return Testimony {}
 }
 
+// Testify testifies against the accomplice 
 func (prisoner *Prisoner) Testify(accomplice *Prisoner) Testimony {
   return Testimony { Suspect: accomplice }
 }
 
+// Respond responds a testimony
 func (prisoner *Prisoner) Respond(interrogation Interrogation) Testimony {
   return prisoner.Strategy.Respond(interrogation)
 }
