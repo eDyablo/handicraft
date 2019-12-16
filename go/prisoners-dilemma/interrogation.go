6c6a563ec3167c7bcc77a0ecb413022a0fcff4f2
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-12-16 14:08:24 +0200

    interrogation implements stringer

--- go/prisoners-dilemma/interrogation.go
+++ go/prisoners-dilemma/interrogation.go
@@ -1,7 +1,16 @@
 package dilemma
 
+import (
+	"fmt"
+)
+
 type Interrogation struct {
   Witness *Prisoner
   Accomplice *Prisoner
   Suspect *Prisoner
+}
+
+func (interrogation Interrogation) String() string {
+  return fmt.Sprintf("[%s is witness, %s is accomplice]",
+    interrogation.Witness, interrogation.Accomplice)
 }
