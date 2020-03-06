8f74f479bc8bceee06a475b8311e49f0f6101515
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-06 15:21:30 +0200

    display help

--- groovy/somaplus/src/main/org/somaplus/domain/CommandControllerActor.groovy
+++ groovy/somaplus/src/main/org/somaplus/domain/CommandControllerActor.groovy
@@ -9,6 +9,15 @@ class CommandControllerActor extends Actor {
   }
 
   void handleCommandEnteredMessage(message) {
+    switch(message.command) {
+      case 'help':
+      system.send(new DisplayTextMessage(text: '''\
+      Commands:
+        help    Display help information
+        quit    Exit the application
+      '''.stripIndent().trim()))
+      break
+    }
     if (message.command != 'quit') {
       system.send(new AskCommandMessage())
     }
