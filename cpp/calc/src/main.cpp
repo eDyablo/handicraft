27cb7a142f830dff2dd1bd0415aaaba9d65df5eb
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-04-16 11:23:00 +0300

    tidy up code

--- cpp/calc/src/main.cpp
+++ cpp/calc/src/main.cpp
@@ -10,9 +10,10 @@ int main(int argc, char const* argv[]) {
   using namespace calc;
   if (argc > 1) {
     cout << argv[1] << "\n";
-    auto tokenizer = tokenizer_t<char>();
-    auto tokens = vector<token_t<char>>();
 
+    auto tokenizer = tokenizer_t<char>();
+
+    auto tokens = vector<token_t<char>>();
     tokenizer.tokenize(&argv[1][0], &argv[1][strlen(argv[1])],
       back_inserter(tokens));
 
