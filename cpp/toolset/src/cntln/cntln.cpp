fc31d1c1556bd89cc5125d5c8aaaf56b9da578d3
eDyablo <eyablonsky@gmail.com>
2020-04-24 13:31:21 +0300

    extract basic type

--- cpp/toolset/src/cntln/cntln.cpp
+++ cpp/toolset/src/cntln/cntln.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 template<typename C>
-struct line_t {
+struct basic_line_t {
   typedef C char_t;
   typedef std::basic_string<char_t> text_t;
   typedef std::basic_istream<char_t> istream_t;
@@ -13,15 +13,17 @@ struct line_t {
 
   text_t text;
 
-  friend istream_t& operator>>(istream_t& input, line_t& line) {
+  friend istream_t& operator>>(istream_t& input, basic_line_t& line) {
     std::getline(input, line.text);
     return input;
   }
 };
 
+typedef basic_line_t<char> line_t;
+
 int main() {
   using namespace std;
-  cout << accumulate(istream_iterator<line_t<char>>(cin), istream_iterator<line_t<char>>(),
+  cout << accumulate(istream_iterator<line_t>(cin), istream_iterator<line_t>(),
     0, bind(plus<size_t>(), placeholders::_1, 1)) << endl;
   return 0;
 }
