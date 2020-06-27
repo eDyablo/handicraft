b48e926671eeefa64818b821eeb13cbadcccc86e
eDyablo <eyablonsky@gmail.com>
2020-06-27 14:16:06 +0300

    tidy up test

--- cpp/coin/sldwnd/test.cpp
+++ cpp/coin/sldwnd/test.cpp
@@ -2,10 +2,18 @@
 
 #include "sliding_window.hpp"
 
+void test(size_t expected, size_t actual) {
+  using namespace std;
+  if (actual != expected) {
+    cout << "expected: " << expected << endl
+      << "actual: " << actual << endl << endl;
+  }
+}
+
 int main() {
   using namespace std;
   using namespace coin::sliding_window;
-  cout << find_max_sum_subarray(3u, vector<int> {2, 1, 5, 1, 3, 2}) << endl
-    << find_max_sum_subarray(2u, vector<int> {2, 3, 4, 1, 5}) << endl;
+  test(9, find_max_sum_subarray(3u, vector<int> {2, 1, 5, 1, 3, 2}));
+  test(7, find_max_sum_subarray(2u, vector<int> {2, 3, 4, 1, 5}));
   return 0;
 }
