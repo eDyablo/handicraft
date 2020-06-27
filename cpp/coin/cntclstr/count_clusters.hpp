6cdce2bcc89fe3623342c13382f7419be9e1515d
eDyablo <eyablonsky@gmail.com>
2020-06-27 10:21:29 +0300

    more tests

--- cpp/coin/cntclstr/count_clusters.hpp
+++ cpp/coin/cntclstr/count_clusters.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <array>
+#include <algorithm>
 
 namespace coin {
-  using ostream = std::ofstream;
-
   template<size_t rows, size_t columns>
-  ostream& operator<<(ostream& stream, int (&grid)[rows][columns]) {
+  std::ostream& operator<<(std::ostream& stream, int (&grid)[rows][columns]) {
     using namespace std;
     for (auto& row : grid) {
       copy(&row[0], &row[columns], ostream_iterator<int>(cout, " "));
@@ -17,7 +16,31 @@ namespace coin {
 
   template<size_t rows, size_t columns>
   size_t count_clusters(int (&grid)[rows][columns]) {
-    size_t count = 0;
-    return count;
+    for (size_t row = 0; row < rows; ++row) {
+      for (size_t col = 0; col < columns; ++col) {
+        auto& cell_value = grid[row][col];
+        if (cell_value > 0 && cell_value < 3) {
+          cell_value = 2;
+          if (col > 0) {
+            auto& left_cell = grid[row][col - 1];
+            if (left_cell == 1) left_cell = 3;
+          }
+          if (row > 0) {
+            auto& above_cell = grid[row - 1][col];
+            if (above_cell == 1) above_cell = 3;
+          }
+          if (col < columns - 1) {
+            auto& right_cell = grid[row][col + 1];
+            if (right_cell == 1) right_cell = 3;
+          }
+          if (row < row - 1) {
+            auto& below_cell = grid[row + 1][col];
+            if (below_cell == 1) below_cell = 3;
+          }
+        }
+      }
+    }
+    using namespace std;
+    return count_if(&grid[0][0], &grid[rows][columns], [](int value) { return value == 2; });
   }
 }
--- cpp/coin/cntclstr/count_clusters_test.cpp
+++ cpp/coin/cntclstr/count_clusters_test.cpp
@@ -4,17 +4,128 @@
 #include <iterator>
 #include <iostream>
 
-
+template<size_t rows, size_t columns>
+void test(int (&grid)[rows][columns], size_t expected_cluster_count) {
+  using namespace coin;
+  using namespace std;
+  auto const cluster_count = count_clusters(grid);
+  if (cluster_count != expected_cluster_count) {
+    cout << grid
+      << "expected " << expected_cluster_count << " custer(s)" << endl
+      << "found " << cluster_count << " cluster(s)" << endl << endl;
+  }
+}
 
 int main() {
-  using namespace std;
-  using namespace coin;
-  int grid[][5] = {
-    {1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 1}
-  };
-  cout << count_clusters(grid) << endl << grid << endl;
+  {
+    int grid[][1] = {
+      {0}
+    };
+    test(grid, 0);
+  }
+  {
+    int grid[][1] = {
+      {1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {1, 0},
+      {0, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 1},
+      {0, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 0},
+      {1, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 0},
+      {0, 1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {1, 1},
+      {0, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 0},
+      {1, 1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {1, 0},
+      {1, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 1},
+      {0, 1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {1, 1},
+      {1, 0}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {1, 1},
+      {0, 1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][2] = {
+      {0, 1},
+      {1, 1}
+    };
+    test(grid, 1);
+  }
+  {
+    int grid[][5] = {
+      {1, 1, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 1, 1}
+    };
+    test(grid, 2);
+  }
+  {
+    int grid[][7] = {
+      {1, 0, 0, 0, 0, 0, 0},
+      {0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 1, 0, 0, 0, 0},
+      {0, 0, 0, 1, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0},
+      {0, 0, 0, 0, 0, 1, 0},
+      {0, 0, 0, 0, 0, 0, 1},
+    };
+    test(grid, 7);
+  }
   return 0;
 }
