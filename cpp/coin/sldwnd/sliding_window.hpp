b8153ca9ebe80b57a6e973c8081c9537cc1a871e
eDyablo <eyablonsky@gmail.com>
2020-06-27 14:49:39 +0300

    find min subarray

--- cpp/coin/sldwnd/sliding_window.hpp
+++ cpp/coin/sldwnd/sliding_window.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <limits>
 #include <vector>
 
 namespace coin {
@@ -20,6 +21,24 @@ namespace coin {
         max_sum = std::max(max_sum, subarray_sum);
       }
       return max_sum;
+    }
+
+    template<typename Element>
+    auto find_min_subarray(Element min_sum, std::vector<Element> const& array) {
+      using namespace std;
+      auto const begin = array.begin();
+      auto const end = array.end();
+      size_t min_subarray_size = array.size();
+      auto subarray_sum = 0;
+      for (auto subarray_begin = begin, subarray_end = begin; subarray_end != end; ++subarray_end) {
+        subarray_sum += *subarray_end;
+        for (; subarray_sum >= min_sum; ++subarray_begin) {
+          subarray_sum -= *subarray_begin;
+          min_subarray_size = min(min_subarray_size,
+            size_t(distance(subarray_begin, subarray_end)) + 1);
+        }
+      }
+      return min_subarray_size;
     }
   }
 }
--- cpp/coin/sldwnd/test.cpp
+++ cpp/coin/sldwnd/test.cpp
@@ -13,7 +13,10 @@ void test(size_t expected, size_t actual) {
 int main() {
   using namespace std;
   using namespace coin::sliding_window;
-  test(9, find_max_sum_subarray(3u, vector<int> {2, 1, 5, 1, 3, 2}));
-  test(7, find_max_sum_subarray(2u, vector<int> {2, 3, 4, 1, 5}));
+  test(9, find_max_sum_subarray<int>(3u, {2, 1, 5, 1, 3, 2}));
+  test(7, find_max_sum_subarray<int>(2u, {2, 3, 4, 1, 5}));
+  test(2, find_min_subarray<int>(7, {2, 1, 5, 2, 3, 2}));
+  test(1, find_min_subarray<int>(7, {2, 1, 5, 2, 8}));
+  test(3, find_min_subarray<int>(8, {3, 4, 1, 1, 6}));
   return 0;
 }
