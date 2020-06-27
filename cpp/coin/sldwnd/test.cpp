5ce3d5be006af48aa61de6ef7c6887563c7a48e8
eDyablo <eyablonsky@gmail.com>
2020-06-27 14:54:49 +0300

    update literals

--- cpp/coin/sldwnd/test.cpp
+++ cpp/coin/sldwnd/test.cpp
@@ -13,10 +13,10 @@ void test(size_t expected, size_t actual) {
 int main() {
   using namespace std;
   using namespace coin::sliding_window;
-  test(9, find_max_sum_subarray<int>(3u, {2, 1, 5, 1, 3, 2}));
-  test(7, find_max_sum_subarray<int>(2u, {2, 3, 4, 1, 5}));
-  test(2, find_min_subarray<int>(7, {2, 1, 5, 2, 3, 2}));
-  test(1, find_min_subarray<int>(7, {2, 1, 5, 2, 8}));
-  test(3, find_min_subarray<int>(8, {3, 4, 1, 1, 6}));
+  test(9u, find_max_sum_subarray<int>(3u, {2, 1, 5, 1, 3, 2}));
+  test(7u, find_max_sum_subarray<int>(2u, {2, 3, 4, 1, 5}));
+  test(2u, find_min_subarray<int>(7, {2, 1, 5, 2, 3, 2}));
+  test(1u, find_min_subarray<int>(7, {2, 1, 5, 2, 8}));
+  test(3u, find_min_subarray<int>(8, {3, 4, 1, 1, 6}));
   return 0;
 }
