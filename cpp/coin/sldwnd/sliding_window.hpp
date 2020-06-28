d9d065efdd31a7f306ab11e362818cfde71a2880
eDyablo <eyablonsky@gmail.com>
2020-06-28 11:20:53 +0300

    tidy up code

--- cpp/coin/sldwnd/sliding_window.hpp
+++ cpp/coin/sldwnd/sliding_window.hpp
@@ -11,13 +11,14 @@ namespace coin {
     template<typename Element>
     auto find_max_sum_subarray(size_t subarray_size, std::vector<Element> const& array) {
       size_t subarray_sum = 0;
-      auto const begin = array.begin();
-      auto const end = array.end();
-      for (auto iter = begin; iter != begin + subarray_size; ++iter) {
+      auto const array_begin = begin(array);
+      auto const array_end = end(array);
+      for (auto iter = array_begin; iter != array_begin + subarray_size; ++iter) {
         subarray_sum += *iter;
       }
       size_t max_sum = 0;
-      for (auto subarray_begin = begin, subarray_end = begin + subarray_size; subarray_end != end;
+      for (auto subarray_begin = array_begin, subarray_end = array_begin + subarray_size;
+      subarray_end != array_end;
         ++subarray_begin, ++subarray_end) {
         subarray_sum += *subarray_end - *subarray_begin;
         max_sum = std::max(max_sum, subarray_sum);
@@ -28,11 +29,12 @@ namespace coin {
     template<typename Element>
     auto find_min_subarray(Element min_sum, std::vector<Element> const& array) {
       using namespace std;
-      auto const begin = array.begin();
-      auto const end = array.end();
-      size_t min_subarray_size = array.size();
+      auto const array_begin = begin(array);
+      auto const array_end = end(array);
+      size_t min_subarray_size = size(array);
       auto subarray_sum = 0;
-      for (auto subarray_begin = begin, subarray_end = begin; subarray_end != end; ++subarray_end) {
+      for (auto subarray_begin = array_begin, subarray_end = array_begin;
+      subarray_end != array_end; ++subarray_end) {
         subarray_sum += *subarray_end;
         for (; subarray_sum >= min_sum; ++subarray_begin) {
           subarray_sum -= *subarray_begin;
@@ -45,15 +47,15 @@ namespace coin {
 
     template<typename Char>
     auto find_longest_distinct_substring_length(size_t max_distinct_chars,
-    std::basic_string<Char> const& text) {
+    std::basic_string<Char> const& string) {
       using namespace std;
       valarray<size_t> char_count(numeric_limits<Char>::max() - numeric_limits<Char>::min() + 1);
       auto const above_zero = [](size_t item) { return item > 0u; };
       auto max_substring_length = numeric_limits<size_t>::lowest();
-      auto const text_begin = text.begin();
-      auto const text_end = text.end();
-      for (auto substring_begin = text_begin, substring_end = text_begin;
-      substring_end != text_end; ++substring_end) {
+      auto const string_begin = begin(string);
+      auto const string_end = end(string);
+      for (auto substring_begin = string_begin, substring_end = string_begin;
+      substring_end != string_end; ++substring_end) {
         ++char_count[*substring_end];
         for (; size_t(count_if(begin(char_count), end(char_count), above_zero)) >
         max_distinct_chars; ++substring_begin) {
@@ -71,8 +73,8 @@ namespace coin {
       valarray<size_t> item_count(numeric_limits<Item>::max() - numeric_limits<Item>::min() + 1);
       auto const above_zero = [](size_t item) { return item > 0u; };
       auto max_subarray_size = numeric_limits<size_t>::lowest();
-      auto const array_begin = array.begin();
-      auto const array_end = array.end();
+      auto const array_begin = begin(array);
+      auto const array_end = end(array);
       for (auto subarray_begin = array_begin, subarray_end = array_begin;
       subarray_end != array_end; ++subarray_end) {
         ++item_count[*subarray_end];
