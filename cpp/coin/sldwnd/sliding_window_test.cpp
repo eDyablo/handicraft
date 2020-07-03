46c20f9886f41897a9d7ecd111ce2e6204bc288b
eDyablo <eyablonsky@gmail.com>
2020-07-03 14:02:58 +0300

    update tests

--- cpp/coin/sldwnd/sliding_window_test.cpp
+++ cpp/coin/sldwnd/sliding_window_test.cpp
@@ -76,7 +76,7 @@ TEST(sliding_window, find_smallest_all_letters_substring) {
   using namespace ::testing;
   EXPECT_THAT(find_smallest_all_letters_substring<char>("abc", "aabdec"),
               Eq("abdec"));
-  EXPECT_THAT(find_smallest_all_letters_substring<char>("abc", "abdabcaxy"),
+  EXPECT_THAT(find_smallest_all_letters_substring<char>("abc", "abdabca"),
               Eq("abc"));
   EXPECT_THAT(find_smallest_all_letters_substring<char>("abc", "adcad"),
               Eq(""));
