642155bf7a375405a0556fdde26df1e9c53a8042
eDyablo <eyablonsky@gmail.com>
2020-11-13 18:24:14 +0200

    use test fixture

--- cpp/coin/prfxtr/prefix_tree_test.cpp
+++ cpp/coin/prfxtr/prefix_tree_test.cpp
@@ -5,21 +5,22 @@
 using namespace ::coin::prfxtr;
 using namespace ::testing;
 
-TEST(tree, has_two_nodes_when_first_letter_inserted) {
+struct char_tree: public Test {
   tree_t<char> tree;
+};
+
+TEST_F(char_tree, has_two_nodes_when_first_letter_inserted) {
   tree.insert("a");
   EXPECT_THAT(tree.node_count(), Eq(2));
 }
 
-TEST(tree, has_two_nodes_when_first_two_equal_letters_inserted) {
-  tree_t<char> tree;
+TEST_F(char_tree, has_two_nodes_when_first_two_equal_letters_inserted) {
   tree.insert("a");
   tree.insert("a");
   EXPECT_THAT(tree.node_count(), Eq(2));
 }
 
-TEST(tree, has_three_nodes_when_two_different_one_letter_words_inserted_two_times_each) {
-  tree_t<char> tree;
+TEST_F(char_tree, has_three_nodes_when_two_different_one_letter_words_inserted_two_times_each) {
   tree.insert("b");
   tree.insert("a");
   tree.insert("a");
@@ -27,14 +28,12 @@ TEST(tree, has_three_nodes_when_two_different_one_letter_words_inserted_two_time
   EXPECT_THAT(tree.node_count(), Eq(3));
 }
 
-TEST(tree, has_five_nodes_when_one_word_with_four_unique_letters_inserted) {
-  tree_t<char> tree;
+TEST_F(char_tree, has_five_nodes_when_one_word_with_four_unique_letters_inserted) {
   tree.insert("abcd");
   EXPECT_THAT(tree.node_count(), Eq(5));
 }
 
-TEST(tree, has_three_nodes_when_two_identical_two_letters_words_inserted) {
-  tree_t<char> tree;
+TEST_F(char_tree, has_three_nodes_when_two_identical_two_letters_words_inserted) {
   tree.insert("ab");
   tree.insert("ab");
   EXPECT_THAT(tree.node_count(), Eq(3));
