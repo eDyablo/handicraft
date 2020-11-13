bdd2ea25de1c4c11cb48577dead95bf282122a85
eDyablo <eyablonsky@gmail.com>
2020-11-13 14:36:33 +0200

    prefix tree has two nodes when first two equal letters inserted

--- cpp/coin/prfxtr/prefix_tree.hpp
+++ cpp/coin/prfxtr/prefix_tree.hpp
@@ -9,6 +9,16 @@ namespace coin {
       using word_t = std::basic_string<element_t>;
 
       struct node_t {
+        bool has(element_t element) const {
+          return element == this->element;
+        }
+
+        void link(element_t element) {
+          this->element = element;
+        }
+
+      private:
+        element_t element;
       };
 
       using node_set_t = std::vector<node_t>;
@@ -17,7 +27,10 @@ namespace coin {
       }
 
       void insert(word_t const& word) {
-        nodes.push_back(node_t());
+        if (nodes[0].has(word[0]) == false) {
+          nodes[0].link(word[0]);
+          nodes.push_back(node_t());
+        }
       }
 
       size_t node_count() const {
--- cpp/coin/prfxtr/prefix_tree_test.cpp
+++ cpp/coin/prfxtr/prefix_tree_test.cpp
@@ -5,8 +5,15 @@
 using namespace ::coin::prfxtr;
 using namespace ::testing;
 
-TEST(tree, has_two_nodes_when_one_letter_inserted) {
+TEST(tree, has_two_nodes_when_first_letter_inserted) {
   tree_t<char> tree;
+  tree.insert("a");
+  EXPECT_THAT(tree.node_count(), Eq(2));
+}
+
+TEST(tree, has_two_nodes_when_first_two_equal_letters_inserted) {
+  tree_t<char> tree;
+  tree.insert("a");
   tree.insert("a");
   EXPECT_THAT(tree.node_count(), Eq(2));
 }
