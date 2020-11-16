b2a44fb91ed73c045e51bb5d1eec809b5aa52493
eDyablo <eyablonsky@gmail.com>
2020-11-16 13:21:42 +0200

    refactor plndrm

--- cpp/coin/plndrm/palindrome.hpp
+++ cpp/coin/plndrm/palindrome.hpp
@@ -3,8 +3,6 @@
 namespace coin {
   namespace plndrm {
     bool is_palindrome(std::string const& string) {
-      if (string.size() < 2)
-        return true;
       for (auto left = string.begin(), right = string.end(); left < --right; ++left) {
         if (*left != *right) return false;
       }
