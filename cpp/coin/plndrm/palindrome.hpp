ce95b312e5c8c1af217aa3bff280cbb4e9453272
eDyablo <eyablonsky@gmail.com>
2020-11-16 11:58:21 +0200

    string of two different letters is not a palindrome

--- cpp/coin/plndrm/palindrome.hpp
+++ cpp/coin/plndrm/palindrome.hpp
@@ -3,7 +3,9 @@
 namespace coin {
   namespace plndrm {
     bool is_palindrome(std::string const& string) {
-      return true;
+      if (string.empty())
+        return true;
+      return false;
     }
   }
 }
--- cpp/coin/plndrm/palindrome_test.cpp
+++ cpp/coin/plndrm/palindrome_test.cpp
@@ -8,3 +8,7 @@ using namespace ::testing;
 TEST(is_palindrome, returns_true_for_empty_string) {
   EXPECT_THAT(is_palindrome(""), Eq(true));
 }
+
+TEST(is_palindrome, returns_flase_for_string_of_two_different_letters) {
+  EXPECT_THAT(is_palindrome("ab"), Eq(false));
+}
