133ecd1cea5b81ed5fc936f2c03d3f8ecaa68943
eDyablo <eyablonsky@gmail.com>
2021-02-25 11:22:32 +0200

    add microsoft coin performance test

--- cpp/coin/microsoft/microsoft_test.cpp
+++ cpp/coin/microsoft/microsoft_test.cpp
@@ -2,6 +2,9 @@
 
 #include <gmock/gmock.h>
 
+#include <algorithm>
+#include <vector>
+
 using namespace ::coin::microsoft;
 using namespace ::std;
 using namespace ::testing;
@@ -11,4 +14,13 @@ TEST(microsoft, max_sum_of_equal_digits_sum_pair) {
   EXPECT_THAT(max_sum_of_equal_digits_sum_pair<int>({42, 33, 60}), Eq(102));
   EXPECT_THAT(max_sum_of_equal_digits_sum_pair<int>({51, 32, 43}), Eq(-1));
   EXPECT_THAT(max_sum_of_equal_digits_sum_pair<int>({}), Eq(-1));
+}
+
+TEST(microsoft, max_sum_of_equal_digits_sum_pair_performance) {
+  vector<int> numbers(200'000);
+  generate(begin(numbers), end(numbers), []() -> int {
+    static int counter = 1'000'000'000;
+    return counter--;
+  });
+  EXPECT_THAT(max_sum_of_equal_digits_sum_pair(numbers), Not(Eq(-1)));
 }
