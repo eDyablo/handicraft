81c66ae30292d7e592b872d363948e3bd721b8b5
eDyablo <eyablonsky@gmail.com>
2021-02-25 09:13:12 +0200

    update prfxtbl coin project

--- cpp/coin/prfxtbl/prefix_table.hpp
+++ cpp/coin/prfxtbl/prefix_table.hpp
@@ -5,8 +5,22 @@ namespace coin {
   namespace prfxtbl {
     template< typename I>
     auto make_table(I begin, I end) {
+      using namespace std;
       auto const size = std::distance(begin, end) - 1;
-      return std::vector<size_t>(size);
+      auto table = std::vector<size_t>(size);
+      auto prefix_right = begin;
+      auto suffix_left = begin; ++suffix_left;
+      auto table_iter = table.begin();
+      for (; suffix_left != end; ++suffix_left, ++table_iter) {
+        while (prefix_right != begin and *prefix_right != *suffix_left) {
+          prefix_right = begin + 
+        }
+        if (*prefix_right == *suffix_left) {
+          ++prefix_right;
+        }
+        *table_iter = distance(begin, prefix_right);
+      }
+      return table;
     }
 
     template<typename E>
--- cpp/coin/prfxtbl/prefix_table_test.cpp
+++ cpp/coin/prfxtbl/prefix_table_test.cpp
@@ -5,6 +5,26 @@
 using namespace ::coin::prfxtbl;
 using namespace ::testing;
 
+std::vector<int> prefixFunction(std::string const& s) {
+  std::vector<int> p(s.size());
+  int j = 0;
+  for (int i = 1; i < (int)s.size(); i++) {
+    while (j > 0 && s[j] != s[i])
+      j = p[j-1];
+
+    if (s[j] == s[i])
+      j++;
+    p[i] = j;
+  }   
+  return p;
+}
+
 TEST(prefix_table, is_empty_for_empty_string) {
   EXPECT_THAT(make(""), IsEmpty());
+}
+
+TEST(prefix_table, rename_me) {
+  EXPECT_THAT(make("a"), ElementsAreArray(prefixFunction("a")));
+  EXPECT_THAT(make("aa"), ElementsAreArray(prefixFunction("aa")));
+  //EXPECT_THAT(make("aaa"), ElementsAreArray(prefixFunction("aaa")));
 }
