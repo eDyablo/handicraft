eaf1ac399ea5bb99f12da2f3bc2997d17b145157
eDyablo <eyablonsky@gmail.com>
2021-02-26 16:30:18 +0200

    tidy up code

--- cpp/coin/microsoft/microsoft.hpp
+++ cpp/coin/microsoft/microsoft.hpp
@@ -45,17 +45,19 @@ namespace coin {
     std::string replace_question_marks(std::string const& riddle) {
       using namespace std;
       string answer = riddle;
-      if (size(answer) > 0) {
+      auto const length = size(answer);
+      if (length > 0) {
         if (answer[0] == '?') {
           answer[0] = 'b';
         }
-        for (size_t index = 1; index < size(answer); ++index) {
-          if (answer[index - 1] == answer[index]) {
-            answer[index - 1] -= 1;
-          } else if (answer[index] == '?') {
-            auto const next = answer[index - 1] + 2;
-            answer[index] = next > 'z' ? 'a' : next;
-          };
+        for (size_t index = 1; index < length; ++index) {
+          auto& prev = answer[index - 1];
+          auto& curr = answer[index];
+          if (prev == curr) {
+            prev -= 1;
+          } else if (curr == '?') {
+            curr = prev + 2 > 'z' ? 'a' : prev + 2;
+          }
         }
       }
       return answer;
