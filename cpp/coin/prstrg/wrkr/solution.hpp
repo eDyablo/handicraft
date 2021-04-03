d0d50b2db86470598f94435edab8ce86e7008d9a
eDyablo <eyablonsky@gmail.com>
2021-04-03 18:52:05 +0300

    multiworker code with issue

--- cpp/coin/prstrg/wrkr/solution.hpp
+++ cpp/coin/prstrg/wrkr/solution.hpp
@@ -1,40 +1,51 @@
 #include <algorithm>
 #include <functional>
+#include <thread>
 #include <vector>
 
 namespace coin {
   namespace prstrg {
     using work_t = std::function<void()>;
-    using completion_t = std::function<void()>;
+    using completion_t = std::function<void(size_t)>;
+    using thread_t = std::thread;
 
     struct worker_t {
-      worker_t(work_t work) : work(work) {}
+      worker_t(work_t work, size_t id = 0) : work(work), id(id) {}
 
       void start(completion_t completion) {
-        work();
-        completion();
+        auto id = this->id;
+        auto action = work;
+        auto thread = thread_t([id, action, completion]() {
+          for (size_t i = 0; i < 5; ++i) {
+            action();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+          }
+          completion(id);
+        });
+        thread.detach();
       }
 
      private:
       work_t work;
+      size_t id;
     };
 
     struct multiworker_t {
-      void add(work_t work) { workers.emplace_back(work); }
+      void add(work_t work) { workers.emplace_back(work, size(workers) + 1); }
 
       void start(completion_t when_done) {
         using namespace std;
         completion = when_done;
         in_progress_count = size(workers);
         for (auto& worker : workers) {
-          worker.start([this]() { all_completed(); });
+          worker.start([this](size_t id) { all_completed(id); });
         }
       }
 
      private:
-      void all_completed() {
+      void all_completed(size_t id) {
         if (--in_progress_count == 0) {
-          completion();
+          completion(id);
         }
       }
 
--- cpp/coin/prstrg/wrkr/test.cpp
+++ cpp/coin/prstrg/wrkr/test.cpp
@@ -1,6 +1,9 @@
 #include <gmock/gmock.h>
 
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "solution.hpp"
 
@@ -10,13 +13,25 @@ using namespace ::testing;
 
 TEST(wrkr, correcteness) {
   auto worker = worker_t{[]() {}};
-  worker.start([]() {});
+  worker.start([](size_t) {});
   EXPECT_THAT(0, Eq(0));
 }
 
 TEST(wrks, multiworker) {
   auto worker = multiworker_t();
-  worker.add([]() { cout << "first" << endl; });
-  worker.add([]() { cout << "second" << endl; });
-  worker.start([] { cout << "all done" << endl; });
+  bool completed = false;
+  worker.add([]() { cout << "first\n"; });
+  worker.add([]() { cout << "second\n"; });
+  worker.add([]() { cout << "third\n"; });
+  worker.add([]() { cout << "fourth\n"; });
+  worker.add([]() { cout << "fifth\n"; });
+  worker.start([&completed](size_t id) {
+    cout << "all done (" << id << ")" << endl << flush;
+    completed = true;
+  });
+  size_t count = 10000;
+  while (not completed and count > 0) {
+    this_thread::sleep_for(chrono::microseconds(1));
+  }
+  cout << endl;
 }
