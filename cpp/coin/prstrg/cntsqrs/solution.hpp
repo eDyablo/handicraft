28e33a73bf3a7edf10f7e5a412d7c012f42d2398
eDyablo <eyablonsky@gmail.com>
2021-04-03 21:04:36 +0300

    add is_square function

--- cpp/coin/prstrg/cntsqrs/solution.hpp
+++ cpp/coin/prstrg/cntsqrs/solution.hpp
@@ -1,4 +1,7 @@
+#include <algorithm>
 #include <cmath>
+#include <set>
+#include <vector>
 
 namespace coin {
   namespace prstrg {
@@ -7,10 +10,29 @@ namespace coin {
       int y;
     };
 
+    struct distance_less {
+      float threshold = 0.001;
+      bool operator()(float const& first, float const& second) const {
+        return second - first > threshold;
+      }
+    };
+
+    using point_vec_t = std::vector<point_t>;
+
     float distance(point_t const& first, point_t const& second) {
       auto const x_delta = first.x - second.x;
       auto const y_delta = first.y - second.y;
       return sqrt(x_delta * x_delta + y_delta * y_delta);
+    }
+
+    bool is_square(point_vec_t const& points) {
+      auto distances = std::set<float, distance_less>();
+      for (size_t i = 0; i < size(points); ++i) {
+        for (size_t j = i + 1; j < size(points); ++j) {
+          distances.insert(distance(points[i], points[j]));
+        }
+      }
+      return size(distances) == 2;
     }
   }  // namespace prstrg
 }  // namespace coin
--- cpp/coin/prstrg/cntsqrs/test.cpp
+++ cpp/coin/prstrg/cntsqrs/test.cpp
@@ -10,3 +10,10 @@ TEST(cntsqrs, distance_correctness) {
   EXPECT_THAT(distance(point_t{1, 0}, point_t{0, 0}), Eq(1));
   EXPECT_NEAR(distance(point_t{0, 0}, point_t{1, 1}), 1.41, 0.01);
 }
+
+TEST(cntsqrs, is_square_correctness) {
+  EXPECT_THAT(is_square(point_vec_t{{0, 0}, {0, 2}, {2, 0}, {2, 2}}), Eq(true));
+  EXPECT_THAT(is_square(point_vec_t{{0, 1}, {2, 1}, {1, 0}, {1, 2}}), Eq(true));
+  EXPECT_THAT(is_square(point_vec_t{{1, 0}, {0, 2}, {2, 3}, {3, 1}}), Eq(true));
+  EXPECT_THAT(is_square(point_vec_t{{1, 0}, {3, 4}, {0, 3}, {4, 1}}), Eq(true));
+}
