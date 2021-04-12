c8d617c0fbc853672dc67b9a09578e7d6d6dd5d5
eDyablo <eyablonsky@gmail.com>
2021-04-12 06:54:40 +0300

    add test to mcrstf coin cplfrgs

--- cpp/coin/mcrsft/cplfrgs/test.cpp
+++ cpp/coin/mcrsft/cplfrgs/test.cpp
@@ -8,4 +8,5 @@ using namespace ::coin::mcrsft;
 TEST(cplfrgs, correctness) {
   EXPECT_THAT(get_longest_distance({2, 6, 8, 5}), Eq(3));
   EXPECT_THAT(get_longest_distance({1, 5, 5, 2, 6}), Eq(4));
+  EXPECT_THAT(get_longest_distance({1, 1}), Eq(2));
 }
