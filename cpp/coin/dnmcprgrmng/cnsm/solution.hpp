66769d41f852428b902032d18c73ed055e94c359
eDyablo <eyablonsky@gmail.com>
2021-04-26 09:32:45 +0300

    dynamic programming can sum problem

---
+++ cpp/coin/dnmcprgrmng/cnsm/solution.hpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <vector>
+
+namespace coin {
+  namespace dnmcprgrmng {
+    using numbers_t = std::vector<int>;
+    using can_sum_memo = std::unordered_map<int, bool>;
+
+    bool can_sum(int target, numbers_t const& numbers, can_sum_memo& memo) {
+      if (memo.find(target) != memo.end()) return memo[target];
+      if (target == 0) return true;
+      if (target < 0) return false;
+      for (auto number : numbers) {
+        if (can_sum(target - number, numbers, memo)) {
+          memo[target] = true;
+          return true;
+        }
+      }
+      memo[target] = false;
+      return false;
+    }
+
+    bool can_sum(int target, numbers_t const& numbers) {
+      can_sum_memo memo;
+      return can_sum(target, numbers, memo);
+    }
+  }  // namespace dnmcprgrmng
+}  // namespace coin
