b5e0db6dea88c3fb668500929d38789be5b53671
eDyablo <eyablonsky@gmail.com>
2021-04-30 21:59:40 +0300

    add dynamic programming best sum problem solution

---
+++ cpp/coin/dnmcprgrmng/bstsm/test.cpp
@@ -0,0 +1,16 @@
+#include <gmock/gmock.h>
+
+#include "solution.hpp"
+
+using namespace ::coin::dnmcprgrmng;
+using namespace ::testing;
+
+TEST(bstsm, correctenss) {
+  EXPECT_THAT(best_sum(7, {5, 3, 4, 7}), ElementsAre(7));
+  EXPECT_THAT(best_sum(8, {2, 3, 5}), ElementsAre(5, 3));
+  EXPECT_THAT(best_sum(8, {1, 4, 5}), ElementsAre(4, 4));
+}
+
+TEST(bstsm, performance) {
+  EXPECT_THAT(best_sum(100, {1, 2, 5, 25}), ElementsAre(25, 25, 25, 25));
+}
