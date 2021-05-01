9855ab5d5fca1fb9c4f7abff084e6200e3562f8a
eDyablo <eyablonsky@gmail.com>
2021-05-01 22:12:23 +0300

    fibonacci problem tabulation solution

--- cpp/coin/dnmcprgrmng/fbnc/solution.hpp
+++ cpp/coin/dnmcprgrmng/fbnc/solution.hpp
@@ -1,25 +1,42 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
 
 namespace coin {
   namespace dnmcprgrmng {
-    using fibo_memo = std::unordered_map<size_t, unsigned long>;
+    namespace mztn {
+      using fibo_memo = std::unordered_map<size_t, unsigned long>;
 
-    unsigned long fibo(size_t n, fibo_memo& memo) {
-      if (n == 1 or n == 2) {
-        return 1;
-      }
-      if (memo.find(n) != memo.end()) {
+      unsigned long fibo(size_t n, fibo_memo& memo) {
+        if (n == 1 or n == 2) {
+          return 1;
+        }
+        if (memo.find(n) != memo.end()) {
+          return memo[n];
+        }
+        memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
         return memo[n];
       }
-      memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
-      return memo[n];
-    }
 
-    unsigned long fibo(size_t n) {
-      fibo_memo memo{};
-      return fibo(n, memo);
-    }
-  }  // namespace dnmcprgrmng
+      unsigned long fibo(size_t n) {
+        fibo_memo memo{};
+        return fibo(n, memo);
+      }
+    }  // namespace mztn
+
+    namespace tbltn {
+      unsigned long fibo(size_t n) {
+        using namespace std;
+        std::vector<unsigned long> table(n + 3, 0);
+        table[1] = 1;
+        for (size_t index = 0; index <= n; ++index) {
+          auto const& value = table[index];
+          table[index + 1] += value;
+          table[index + 2] += value;
+        }
+        return table[n];
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/fbnc/test.cpp
+++ cpp/coin/dnmcprgrmng/fbnc/test.cpp
@@ -3,9 +3,11 @@
 #include "solution.hpp"
 
 using namespace ::testing;
+
 using namespace ::coin::dnmcprgrmng;
 
-TEST(fbnc, correctness) {
+TEST(fbnc_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(fibo(1), Eq(1));
   EXPECT_THAT(fibo(2), Eq(1));
   EXPECT_THAT(fibo(3), Eq(2));
@@ -17,6 +19,26 @@ TEST(fbnc, correctness) {
   EXPECT_THAT(fibo(9), Eq(34));
 }
 
-TEST(fbnc, performance) {
+TEST(fbnc_mztn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(fibo(50), Eq(12'586'269'025));
+}
+
+TEST(fbnc_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(fibo(0), Eq(0));
+  EXPECT_THAT(fibo(1), Eq(1));
+  EXPECT_THAT(fibo(2), Eq(1));
+  EXPECT_THAT(fibo(3), Eq(2));
+  EXPECT_THAT(fibo(4), Eq(3));
+  EXPECT_THAT(fibo(5), Eq(5));
+  EXPECT_THAT(fibo(6), Eq(8));
+  EXPECT_THAT(fibo(7), Eq(13));
+  EXPECT_THAT(fibo(8), Eq(21));
+  EXPECT_THAT(fibo(9), Eq(34));
+}
+
+TEST(fbnc_tbltn, performance) {
+  using namespace tbltn;
   EXPECT_THAT(fibo(50), Eq(12'586'269'025));
 }
