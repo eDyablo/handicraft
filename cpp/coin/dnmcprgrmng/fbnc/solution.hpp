e4674f336720ae85bdcf619518aee3bae4d9f9fb
eDyablo <eyablonsky@gmail.com>
2021-05-01 22:13:40 +0300

    tidy up code

--- cpp/coin/dnmcprgrmng/fbnc/solution.hpp
+++ cpp/coin/dnmcprgrmng/fbnc/solution.hpp
@@ -9,12 +9,8 @@ namespace coin {
       using fibo_memo = std::unordered_map<size_t, unsigned long>;
 
       unsigned long fibo(size_t n, fibo_memo& memo) {
-        if (n == 1 or n == 2) {
-          return 1;
-        }
-        if (memo.find(n) != memo.end()) {
-          return memo[n];
-        }
+        if (n == 1 or n == 2) return 1;
+        if (memo.find(n) != memo.end()) return memo[n];
         memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
         return memo[n];
       }
