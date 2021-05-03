4adc6ff1e9ca743deec8932b36e0f3ca9dc3fd21
eDyablo <eyablonsky@gmail.com>
2021-05-03 15:07:32 +0300

    how sum problem tabulated solution

--- cpp/coin/dnmcprgrmng/hwsm/solution.hpp
+++ cpp/coin/dnmcprgrmng/hwsm/solution.hpp
@@ -8,38 +8,73 @@ namespace coin {
   namespace dnmcprgrmng {
     using number_t = int;
     using number_set_t = std::vector<number_t>;
-    using how_sum_result_t = std::optional<number_set_t>;
-    using how_sum_memo_t = std::unordered_map<number_t, how_sum_result_t>;
 
-    /**
-     * m = target
-     * n = length of numbers array
-     * time:  O(n * m^2)
-     * space: O(m^2)
-     **/
-    how_sum_result_t how_sum(number_t target, number_set_t const& numbers,
-                          how_sum_memo_t& memo) {
-      using namespace std;
-      if (memo.find(target) != memo.end()) return memo[target];
-      if (target == 0) return number_set_t{};
-      if (target < 0) return std::nullopt;
-      how_sum_result_t combination;
-      for (auto number : numbers) {
-        auto const remainder = target - number;
-        combination = how_sum(remainder, numbers, memo);
-        if (combination.has_value()) {
-          combination.value().push_back(number);
-          memo[target] = combination;
-          return combination;
+    namespace mztn {
+      using how_sum_result_t = std::optional<number_set_t>;
+      using how_sum_memo_t = std::unordered_map<number_t, how_sum_result_t>;
+
+      /**
+       * m = target sum
+       * n = amount of numners
+       *
+       * Memoized solution:
+       * time:  O(n * m^2)
+       * space: O(m^2)
+       **/
+      how_sum_result_t how_sum(number_t target, number_set_t const& numbers,
+                               how_sum_memo_t& memo) {
+        using namespace std;
+        if (memo.find(target) != memo.end()) return memo[target];
+        if (target == 0) return number_set_t{};
+        if (target < 0) return std::nullopt;
+        how_sum_result_t combination;
+        for (auto number : numbers) {
+          auto const remainder = target - number;
+          combination = how_sum(remainder, numbers, memo);
+          if (combination.has_value()) {
+            combination.value().push_back(number);
+            memo[target] = combination;
+            return combination;
+          }
         }
+        memo[target] = combination;
+        return combination;
       }
-      memo[target] = combination;
-      return combination;
-    }
 
-    number_set_t how_sum(number_t target, number_set_t const& numbers) {
-      how_sum_memo_t memo;
-      return how_sum(target, numbers, memo).value_or(number_set_t{});
-    }
-  }  // namespace dnmcprgrmng
+      number_set_t how_sum(number_t target, number_set_t const& numbers) {
+        how_sum_memo_t memo;
+        return how_sum(target, numbers, memo).value_or(number_set_t{});
+      }
+    }  // namespace mztn
+
+    namespace tbltn {
+      using how_sum_record_t = std::optional<number_set_t>;
+      using how_sum_table_t = std::vector<how_sum_record_t>;
+
+      /**
+       * m = target sum
+       * n = amount of numners
+       *
+       * Tabulazed solution:
+       * time:  O(n * m^2)
+       * space: O(m^2)
+       **/
+      number_set_t how_sum(number_t target, number_set_t const& numbers) {
+        how_sum_table_t table(target + 1);
+        table[0] = number_set_t{};
+        for (number_t sub_target = 0; sub_target <= target; ++sub_target) {
+          if (table[sub_target].has_value()) {
+            for (auto const number : numbers) {
+              auto const sum = sub_target + number;
+              if (sum <= target) {
+                table[sum] = table[sub_target];
+                table[sum].value().push_back(number);
+              }
+            }
+          }
+        }
+        return table[target].value_or(number_set_t{});
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/hwsm/test.cpp
+++ cpp/coin/dnmcprgrmng/hwsm/test.cpp
@@ -5,7 +5,8 @@
 using namespace ::coin::dnmcprgrmng;
 using namespace ::testing;
 
-TEST(hwsm, correctness) {
+TEST(hwsm_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(how_sum(0, {1, 2, 3}), IsEmpty());
   EXPECT_THAT(how_sum(1, {1}), ElementsAre(1));
   EXPECT_THAT(how_sum(2, {1, 1}), ElementsAre(1, 1));
@@ -16,4 +17,24 @@ TEST(hwsm, correctness) {
   EXPECT_THAT(how_sum(8, {2, 3, 5}), ElementsAre(2, 2, 2, 2));
 }
 
-TEST(hwsm, performance) { EXPECT_THAT(how_sum(300, {7, 14}), IsEmpty()); }
+TEST(hwsm_mztn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(how_sum(300, {7, 14}), IsEmpty());
+}
+
+TEST(hwsm_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(how_sum(0, {1, 2, 3}), IsEmpty());
+  EXPECT_THAT(how_sum(1, {1}), ElementsAre(1));
+  EXPECT_THAT(how_sum(2, {1}), ElementsAre(1, 1));
+  EXPECT_THAT(how_sum(2, {1, 1}), ElementsAre(1, 1));
+  EXPECT_THAT(how_sum(7, {2, 3}), ElementsAre(3, 2, 2));
+  EXPECT_THAT(how_sum(7, {5, 3, 4, 7}), ElementsAre(4, 3));
+  EXPECT_THAT(how_sum(7, {2, 4}), IsEmpty());
+  EXPECT_THAT(how_sum(8, {2, 3, 5}), ElementsAre(2, 2, 2, 2));
+}
+
+TEST(hwsm_tbltn, performance) {
+  using namespace tbltn;
+  EXPECT_THAT(how_sum(300, {7, 14}), IsEmpty());
+}
