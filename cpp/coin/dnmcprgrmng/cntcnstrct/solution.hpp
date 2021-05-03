085de269e8be9e9af1a00fee4f33df658935d802
eDyablo <eyablonsky@gmail.com>
2021-05-03 18:38:51 +0300

    count construct problem tabulated solution

--- cpp/coin/dnmcprgrmng/cntcnstrct/solution.hpp
+++ cpp/coin/dnmcprgrmng/cntcnstrct/solution.hpp
@@ -9,38 +9,69 @@ namespace coin {
     using text_t = std::string;
     using word_t = std::string;
     using word_bank_t = std::vector<word_t>;
-    using count_construct_memo_t = std::unordered_map<text_t, size_t>;
 
-    /**
-     * m = text length
-     * n = words bank size
-     *
-     * Brute force:
-     * time:  O(n^m * m) == O(n^m)
-     * space: O(m^2)
-     *
-     * Memoized version:
-     * time:  O(n * m^2)
-     * space: O(m^2)
-     **/
-    size_t count_construct(text_t const& text, word_bank_t const& word_bank,
-                           count_construct_memo_t& memo) {
-      if (memo.find(text) != memo.end()) return memo[text];
-      if (text.empty()) return 1;
-      size_t count = 0;
-      for (auto word : word_bank) {
-        if (text.find(word) == 0) {
-          auto const reminder = text.substr(word.size());
-          count += count_construct(reminder, word_bank, memo);
+    namespace mztn {
+      using count_construct_memo_t = std::unordered_map<text_t, size_t>;
+
+      /**
+       * m = text length
+       * n = words bank size
+       *
+       * Brute force:
+       * time:  O(n^m * m) == O(n^m)
+       * space: O(m^2)
+       *
+       * Memoized solution:
+       * time:  O(n * m^2)
+       * space: O(m^2)
+       **/
+      size_t count_construct(text_t const& text, word_bank_t const& word_bank,
+                             count_construct_memo_t& memo) {
+        if (memo.find(text) != memo.end()) return memo[text];
+        if (text.empty()) return 1;
+        size_t count = 0;
+        for (auto word : word_bank) {
+          if (text.find(word) == 0) {
+            auto const reminder = text.substr(word.size());
+            count += count_construct(reminder, word_bank, memo);
+          }
         }
+        memo[text] = count;
+        return count;
       }
-      memo[text] = count;
-      return count;
-    }
 
-    size_t count_construct(text_t const& text, word_bank_t const& word_bank) {
-      count_construct_memo_t memo;
-      return count_construct(text, word_bank, memo);
-    }
-  }  // namespace dnmcprgrmng
+      size_t count_construct(text_t const& text, word_bank_t const& word_bank) {
+        count_construct_memo_t memo;
+        return count_construct(text, word_bank, memo);
+      }
+    }  // namespace mztn
+
+    namespace tbltn {
+      using count_construct_table_t = std::vector<size_t>;
+
+      /**
+       * m = text length
+       * n = words bank size
+       * 
+       * Tabulated solution:
+       * time:  O(n * m^2)
+       * space: O(m)
+       **/
+      size_t count_construct(text_t const& text, word_bank_t const& word_bank) {
+        count_construct_table_t table(text.size() + 1, 0);
+        table[0] = 1;
+        for (size_t position = 0; position < text.size(); ++position) {
+          if (table[position] > 0) {
+            for (auto const& word : word_bank) {
+              auto const remainder = text.substr(position);
+              if (remainder.find(word) == 0) {
+                table[position + word.size()] += table[position];
+              }
+            }
+          }
+        }
+        return table[text.size()];
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/cntcnstrct/test.cpp
+++ cpp/coin/dnmcprgrmng/cntcnstrct/test.cpp
@@ -5,7 +5,8 @@
 using namespace ::coin::dnmcprgrmng;
 using namespace ::testing;
 
-TEST(cntcnstrct, correctness) {
+TEST(cntcnstrct_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(count_construct("", {"a", "ab", "abc"}), Eq(1));
   EXPECT_THAT(count_construct("abc", {"a", "b", "c", "ab", "bc", "abc"}),
               Eq(4));
@@ -21,7 +22,41 @@ TEST(cntcnstrct, correctness) {
               Eq(4));
 }
 
-TEST(cntcnstrct, performance) {
+TEST(cntcnstrct_mzn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+                              {"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}),
+              Eq(0));
+  EXPECT_THAT(count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+                              {"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}),
+              Eq(437513522));
+}
+
+TEST(cntcnstrct_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(count_construct("", {"a"}), Eq(1));
+  EXPECT_THAT(count_construct("a", {"a"}), Eq(1));
+  EXPECT_THAT(count_construct("a", {"b"}), Eq(0));
+  EXPECT_THAT(count_construct("a", {"a", "a"}), Eq(2));
+  EXPECT_THAT(count_construct("ab", {"a"}), Eq(0));
+  EXPECT_THAT(count_construct("ab", {"b", "b"}), Eq(0));
+  EXPECT_THAT(count_construct("aa", {"a", "a"}), Eq(4));
+  EXPECT_THAT(count_construct("abc", {"a", "b", "c", "ab", "bc", "abc"}),
+              Eq(4));
+  EXPECT_THAT(count_construct("abcdef", {"ab", "abc", "cd", "def", "abcd"}),
+              Eq(1));
+  EXPECT_THAT(count_construct("purple", {"purp", "p", "ur", "le", "purpl"}),
+              Eq(2));
+  EXPECT_THAT(count_construct("skateboard",
+                              {"bo", "rd", "ate", "t", "ska", "sk", "boar"}),
+              Eq(0));
+  EXPECT_THAT(count_construct("enterapotentpot",
+                              {"a", "p", "ent", "enter", "ot", "o", "t"}),
+              Eq(4));
+}
+
+TEST(cntcnstrct_tbltn, performance) {
+  using namespace tbltn;
   EXPECT_THAT(count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
                               {"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}),
               Eq(0));
