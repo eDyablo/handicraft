0bae4a27844cdae365267a172fd7d5158ddfb387
eDyablo <eyablonsky@gmail.com>
2021-05-03 17:36:32 +0300

    can construct problem tabulated solution

--- cpp/coin/dnmcprgrmng/cncnstrct/solution.hpp
+++ cpp/coin/dnmcprgrmng/cncnstrct/solution.hpp
@@ -9,40 +9,71 @@ namespace coin {
     using word_t = std::string;
     using text_t = std::string;
     using word_set_t = std::vector<word_t>;
-    using can_construct_memo_t = std::unordered_map<text_t, bool>;
 
-    /**
-     * m = text length
-     * n = words count
-     *
-     * Brute force:
-     * time:  O(n^m * m) == O(n^m)
-     * space: O(m^2)
-     *
-     * Memoized version:
-     * time:  O(n * m^2)
-     * space: O(m^2)
-     **/
-    bool can_construct(text_t const& text, word_set_t const& word_bank,
-                       can_construct_memo_t& memo) {
-      if (memo.find(text) != memo.end()) return memo[text];
-      if (empty(text)) return true;
-      for (auto word : word_bank) {
-        if (text.find(word) == 0) {
-          auto const reminder = text.substr(size(word));
-          if (can_construct(reminder, word_bank, memo)) {
-            memo[text] = true;
-            return true;
+    namespace mztn {
+      using can_construct_memo_t = std::unordered_map<text_t, bool>;
+
+      /**
+       * m = text length
+       * n = amount of words
+       *
+       * Brute force:
+       * time:  O(n^m * m) == O(n^m)
+       * space: O(m^2)
+       *
+       * Memoized solution:
+       * time:  O(n * m^2)
+       * space: O(m^2)
+       **/
+      bool can_construct(text_t const& text, word_set_t const& word_bank,
+                         can_construct_memo_t& memo) {
+        if (memo.find(text) != memo.end()) return memo[text];
+        if (empty(text)) return true;
+        for (auto word : word_bank) {
+          if (text.find(word) == 0) {
+            auto const reminder = text.substr(size(word));
+            if (can_construct(reminder, word_bank, memo)) {
+              memo[text] = true;
+              return true;
+            }
           }
         }
+        memo[text] = false;
+        return false;
       }
-      memo[text] = false;
-      return false;
-    }
 
-    bool can_construct(text_t const& text, word_set_t const& words) {
-      can_construct_memo_t memo;
-      return can_construct(text, words, memo);
-    }
-  }  // namespace dnmcprgrmng
+      bool can_construct(text_t const& text, word_set_t const& words) {
+        can_construct_memo_t memo;
+        return can_construct(text, words, memo);
+      }
+    }  // namespace mztn
+
+    namespace tbltn {
+      using can_construct_t = std::vector<bool>;
+
+      /**
+       * m = text length
+       * n = amount of words
+       *
+       * Tabulated solution:
+       * time:  O(n * m^2)
+       * space: O(m)
+       **/
+      bool can_construct(text_t const& text, word_set_t const& word_bank) {
+        can_construct_t table(text.size() + 1, false);
+        table[0] = true;
+        for (size_t start = 0; start < text.size(); ++start) {
+          if (table[start]) {
+            auto const reminder = text.substr(start);
+            for (auto word : word_bank) {
+              if (reminder.find(word) == 0) {
+                table[start + word.size()] = true;
+              }
+            }
+          }
+        }
+        return table[text.size()];
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/cncnstrct/test.cpp
+++ cpp/coin/dnmcprgrmng/cncnstrct/test.cpp
@@ -7,7 +7,8 @@
 using namespace ::coin::dnmcprgrmng;
 using namespace ::testing;
 
-TEST(cncnstrct, correctness) {
+TEST(cncnstrct_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(can_construct("", {"a", "ab", "abc"}), Eq(true));
   EXPECT_THAT(can_construct("abcdef", {"ab", "abc", "cd", "def", "abcd"}),
               Eq(true));
@@ -25,7 +26,41 @@ TEST(cncnstrct, correctness) {
   EXPECT_THAT(can_construct(alphabet, letters), Eq(true));
 }
 
-TEST(cncnstrct, performance) {
+TEST(cncnstrct_mztn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(can_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+                            {"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}),
+              Eq(false));
+}
+
+TEST(cncnstrct_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(can_construct("", {"a", "ab", "abc"}), Eq(true));
+  EXPECT_THAT(can_construct("a", {"a"}), Eq(true));
+  EXPECT_THAT(can_construct("ab", {"a", "b"}), Eq(true));
+  EXPECT_THAT(can_construct("b", {"a"}), Eq(false));
+  EXPECT_THAT(can_construct("abc", {"ab", "c"}), Eq(true));
+  EXPECT_THAT(can_construct("abc", {"a", "bc"}), Eq(true));
+  EXPECT_THAT(can_construct("abc", {"ab", "bc"}), Eq(false));
+  EXPECT_THAT(can_construct("ababab", {"ab"}), Eq(true));
+  EXPECT_THAT(can_construct("abcdef", {"ab", "abc", "cd", "def", "abcd"}),
+              Eq(true));
+  EXPECT_THAT(can_construct("skateboard",
+                            {"bo", "rd", "ate", "t", "ska", "sk", "boar"}),
+              Eq(false));
+  EXPECT_THAT(can_construct("enterapotentpot",
+                            {"a", "p", "ent", "enter", "ot", "o", "t"}),
+              Eq(true));
+  text_t alphabet = "abcdefghijklmnopqrstuvwxyz";
+  word_set_t letters;
+  using namespace std;
+  transform(begin(alphabet), end(alphabet), back_inserter(letters),
+            [](char l) -> word_t { return word_t{l}; });
+  EXPECT_THAT(can_construct(alphabet, letters), Eq(true));
+}
+
+TEST(cncnstrct_tbltn, performance) {
+  using namespace tbltn;
   EXPECT_THAT(can_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
                             {"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}),
               Eq(false));
