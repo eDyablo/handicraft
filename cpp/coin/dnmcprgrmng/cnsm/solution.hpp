435a702f0d7a8055a35875eb8836faa78640b016
eDyablo <eyablonsky@gmail.com>
2021-05-03 13:39:48 +0300

    can sum problem tabulated solution

--- cpp/coin/dnmcprgrmng/cnsm/solution.hpp
+++ cpp/coin/dnmcprgrmng/cnsm/solution.hpp
@@ -5,25 +5,55 @@ namespace coin {
   namespace dnmcprgrmng {
     using number_t = int;
     using number_set_t = std::vector<int>;
-    using can_sum_memo = std::unordered_map<int, bool>;
 
-    bool can_sum(int target, number_set_t const& numbers, can_sum_memo& memo) {
-      if (memo.find(target) != memo.end()) return memo[target];
-      if (target == 0) return true;
-      if (target < 0) return false;
-      for (auto number : numbers) {
-        if (can_sum(target - number, numbers, memo)) {
-          memo[target] = true;
-          return true;
+    namespace mztn {
+      using can_sum_memo = std::unordered_map<int, bool>;
+
+      bool can_sum(int target, number_set_t const& numbers,
+                   can_sum_memo& memo) {
+        if (memo.find(target) != memo.end()) return memo[target];
+        if (target == 0) return true;
+        if (target < 0) return false;
+        for (auto number : numbers) {
+          if (can_sum(target - number, numbers, memo)) {
+            memo[target] = true;
+            return true;
+          }
         }
+        memo[target] = false;
+        return false;
       }
-      memo[target] = false;
-      return false;
-    }
 
-    bool can_sum(int target, number_set_t const& numbers) {
-      can_sum_memo memo;
-      return can_sum(target, numbers, memo);
-    }
-  }  // namespace dnmcprgrmng
+      bool can_sum(int target, number_set_t const& numbers) {
+        can_sum_memo memo;
+        return can_sum(target, numbers, memo);
+      }
+    }  // namespace mztn
+
+    namespace tbltn {
+      using can_sum_table = std::vector<bool>;
+
+      /**
+       * m = target sum
+       * n = numbers count
+       * 
+       * Tabulated solution:
+       * time:  O(m * n)
+       * space: O(m)
+       */
+      bool can_sum(number_t target, number_set_t const& numbers) {
+        can_sum_table table(target + 1, 0);
+        table[0] = true;
+        for (number_t sub_target = 0; sub_target <= target; ++sub_target) {
+          if (table[sub_target]) {
+            for (auto number : numbers) {
+              auto const sum = sub_target + number;
+              if (sum <= target) table[sum] = true;
+            }
+          }
+        }
+        return table[target];
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/cnsm/test.cpp
+++ cpp/coin/dnmcprgrmng/cnsm/test.cpp
@@ -5,7 +5,8 @@
 using namespace ::testing;
 using namespace ::coin::dnmcprgrmng;
 
-TEST(cnsm, correctness) {
+TEST(cnsm_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(can_sum(0, {}), Eq(true));
   EXPECT_THAT(can_sum(7, {2, 3}), Eq(true));
   EXPECT_THAT(can_sum(7, {5, 3, 4, 7}), Eq(true));
@@ -13,6 +14,25 @@ TEST(cnsm, correctness) {
   EXPECT_THAT(can_sum(8, {2, 3, 5}), Eq(true));
 }
 
-TEST(cnsm, performance) {
+TEST(cnsm_mztn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(can_sum(300, {7, 14}), Eq(false));
+}
+
+TEST(cnsm_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(can_sum(0, {1, 2, 3}), Eq(true));
+  EXPECT_THAT(can_sum(1, {1}), Eq(true));
+  EXPECT_THAT(can_sum(3, {1, 2}), Eq(true));
+  EXPECT_THAT(can_sum(1, {10}), Eq(false));
+  EXPECT_THAT(can_sum(7, {5, 3, 4}), Eq(true));
+  EXPECT_THAT(can_sum(7, {2, 3}), Eq(true));
+  EXPECT_THAT(can_sum(7, {5, 3, 4, 7}), Eq(true));
+  EXPECT_THAT(can_sum(7, {2, 4}), Eq(false));
+  EXPECT_THAT(can_sum(8, {2, 3, 5}), Eq(true));
+}
+
+TEST(cnsm_tbltn, performance) {
+  using namespace tbltn;
   EXPECT_THAT(can_sum(300, {7, 14}), Eq(false));
 }
