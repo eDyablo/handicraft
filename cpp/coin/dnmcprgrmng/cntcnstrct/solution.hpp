9956526626a9d5e139fe8972748b2a55bce8ff05
eDyablo <eyablonsky@gmail.com>
2021-05-04 10:54:43 +0300

    count construct problem brute force solution

--- cpp/coin/dnmcprgrmng/cntcnstrct/solution.hpp
+++ cpp/coin/dnmcprgrmng/cntcnstrct/solution.hpp
@@ -10,9 +10,7 @@ namespace coin {
     using word_t = std::string;
     using word_bank_t = std::vector<word_t>;
 
-    namespace mztn {
-      using count_construct_memo_t = std::unordered_map<text_t, size_t>;
-
+    namespace brtfrc {
       /**
        * m = text length
        * n = words bank size
@@ -20,11 +18,31 @@ namespace coin {
        * Brute force:
        * time:  O(n^m * m) == O(n^m)
        * space: O(m^2)
+       */
+      size_t count_construct(text_t const& text, word_bank_t const& word_bank) {
+        if (text.empty()) return 1;
+        size_t count = 0;
+        for (auto word : word_bank) {
+          if (text.find(word) == 0) {
+            auto const reminder = text.substr(word.size());
+            count += count_construct(reminder, word_bank);
+          }
+        }
+        return count;
+      }
+    }  // namespace brtfrc
+
+    namespace mztn {
+      using count_construct_memo_t = std::unordered_map<text_t, size_t>;
+
+      /**
+       * m = text length
+       * n = words bank size
        *
        * Memoized solution:
        * time:  O(n * m^2)
        * space: O(m^2)
-       **/
+       */
       size_t count_construct(text_t const& text, word_bank_t const& word_bank,
                              count_construct_memo_t& memo) {
         if (memo.find(text) != memo.end()) return memo[text];
@@ -56,7 +74,7 @@ namespace coin {
        * Tabulated solution:
        * time:  O(n * m^2)
        * space: O(m)
-       **/
+       */
       size_t count_construct(text_t const& text, word_bank_t const& word_bank) {
         count_construct_table_t table(text.size() + 1, 0);
         table[0] = 1;
--- cpp/coin/dnmcprgrmng/cntcnstrct/test.cpp
+++ cpp/coin/dnmcprgrmng/cntcnstrct/test.cpp
@@ -5,6 +5,23 @@
 using namespace ::coin::dnmcprgrmng;
 using namespace ::testing;
 
+TEST(cntcnstrct_brtfrc, correctness) {
+  using namespace brtfrc;
+  EXPECT_THAT(count_construct("", {"a", "ab", "abc"}), Eq(1));
+  EXPECT_THAT(count_construct("abc", {"a", "b", "c", "ab", "bc", "abc"}),
+              Eq(4));
+  EXPECT_THAT(count_construct("abcdef", {"ab", "abc", "cd", "def", "abcd"}),
+              Eq(1));
+  EXPECT_THAT(count_construct("purple", {"purp", "p", "ur", "le", "purpl"}),
+              Eq(2));
+  EXPECT_THAT(count_construct("skateboard",
+                              {"bo", "rd", "ate", "t", "ska", "sk", "boar"}),
+              Eq(0));
+  EXPECT_THAT(count_construct("enterapotentpot",
+                              {"a", "p", "ent", "enter", "ot", "o", "t"}),
+              Eq(4));
+}
+
 TEST(cntcnstrct_mztn, correctness) {
   using namespace mztn;
   EXPECT_THAT(count_construct("", {"a", "ab", "abc"}), Eq(1));
