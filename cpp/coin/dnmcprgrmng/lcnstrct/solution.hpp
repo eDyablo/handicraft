7a762b4819abdc2357e91d707e2b5e0ee28b8292
eDyablo <eyablonsky@gmail.com>
2021-05-04 10:31:11 +0300

    all construct problem tabulated solution

--- cpp/coin/dnmcprgrmng/lcnstrct/solution.hpp
+++ cpp/coin/dnmcprgrmng/lcnstrct/solution.hpp
@@ -11,47 +11,87 @@ namespace coin {
     using word_t = std::string;
     using word_bank_t = std::vector<word_t>;
     using combination_set_t = std::vector<word_bank_t>;
-    using all_construct_memo_t = std::unordered_map<text_t, combination_set_t>;
 
-    /**
-     * m = text length
-     * n = word count
-     * 
-     * Memoized solution:
-     * O(n^m)
-     * O(m)
-     */
-    combination_set_t all_construct(text_t const& text,
-                                    word_bank_t const& word_bank,
-                                    all_construct_memo_t& memo) {
-      using namespace std;
-      if (memo.find(text) != memo.end()) return memo[text];
-      if (text.empty()) return combination_set_t{word_bank_t{}};
-      combination_set_t combinations;
-      for (auto word : word_bank) {
-        if (text.find(word) == 0) {
-          auto const reminder = text.substr(word.size());
-          auto reminder_combinations = all_construct(reminder, word_bank, memo);
-          for (auto& words : reminder_combinations) {
-            words.push_back(word);
+    namespace mztn {
+      using all_construct_memo_t =
+          std::unordered_map<text_t, combination_set_t>;
+
+      /**
+       * m = text length
+       * n = word count
+       *
+       * Memoized solution:
+       * O(n^m)
+       * O(m)
+       */
+      combination_set_t all_construct(text_t const& text,
+                                      word_bank_t const& word_bank,
+                                      all_construct_memo_t& memo) {
+        using namespace std;
+        if (memo.find(text) != memo.end()) return memo[text];
+        if (text.empty()) return combination_set_t{word_bank_t{}};
+        combination_set_t combinations;
+        for (auto word : word_bank) {
+          if (text.find(word) == 0) {
+            auto const reminder = text.substr(word.size());
+            auto reminder_combinations =
+                all_construct(reminder, word_bank, memo);
+            for (auto& words : reminder_combinations) {
+              words.push_back(word);
+            }
+            move(begin(reminder_combinations), end(reminder_combinations),
+                 back_inserter(combinations));
           }
-          move(begin(reminder_combinations), end(reminder_combinations),
-               back_inserter(combinations));
         }
+        memo[text] = combinations;
+        return combinations;
       }
-      memo[text] = combinations;
-      return combinations;
-    }
 
-    combination_set_t all_construct(text_t const& text,
-                                    word_bank_t const& word_bank) {
-      using namespace std;
-      all_construct_memo_t memo;
-      auto combinations = all_construct(text, word_bank, memo);
-      for (auto& combination : combinations) {
-        reverse(begin(combination), end(combination));
+      combination_set_t all_construct(text_t const& text,
+                                      word_bank_t const& word_bank) {
+        using namespace std;
+        all_construct_memo_t memo;
+        auto combinations = all_construct(text, word_bank, memo);
+        for (auto& combination : combinations) {
+          reverse(begin(combination), end(combination));
+        }
+        return combinations;
       }
-      return combinations;
-    }
-  }  // namespace dnmcprgrmng
+    }  // namespace mztn
+
+    namespace tbltn {
+      using all_construct_table_t = std::vector<combination_set_t>;
+
+      /**
+       * m = text length
+       * n = word count
+       *
+       * Tabulated solution:
+       * O(n^m)
+       * O(n^m)
+       */
+      combination_set_t all_construct(text_t const& text,
+                                      word_bank_t const& word_bank) {
+        using namespace std;
+        all_construct_table_t table(text.size() + 1);
+        table[0] = combination_set_t{word_bank_t{}};
+        for (size_t position = 0; position < text.size(); ++position) {
+          if (not table[position].empty()) {
+            auto const reminder = text.substr(position);
+            for (auto const& word : word_bank) {
+              if (reminder.find(word) == 0) {
+                auto combination = table[position];
+                for (auto& words : combination) {
+                  words.push_back(word);
+                }
+                move(begin(combination), end(combination),
+                     back_inserter(table[position + word.size()]));
+              }
+            }
+          }
+        }
+        return table[text.size()];
+      }
+    }  // namespace tbltn
+  }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/lcnstrct/test.cpp
+++ cpp/coin/dnmcprgrmng/lcnstrct/test.cpp
@@ -5,7 +5,8 @@
 using namespace ::coin::dnmcprgrmng;
 using namespace ::testing;
 
-TEST(lcntstrct, correctness) {
+TEST(lcntstrct_mztn, correctness) {
+  using namespace mztn;
   EXPECT_THAT(all_construct("hello", {"cat", "dog"}), IsEmpty());
   EXPECT_THAT(all_construct("", {"cat", "dog"}), ElementsAre(word_bank_t{}));
   EXPECT_THAT(all_construct("purple", {"purp", "p", "ur", "le", "purpl"}),
@@ -20,14 +21,35 @@ TEST(lcntstrct, correctness) {
               IsEmpty());
 }
 
-TEST(lcntstrct, perfomance) {
+TEST(lcntstrct_mztn, perfomance) {
+  using namespace mztn;
   EXPECT_THAT(all_construct("aaaaaaaaaaaaaaaaaaaaaaaaaz",
                             {"a", "aa", "aaa", "aaaa", "aaaaa"}),
               IsEmpty());
 }
 
-TEST(lcntstrct, DISABLED_worst_case_performance) {
+TEST(lcntstrct_mztn, DISABLED_worst_case_performance) {
+  using namespace mztn;
   EXPECT_THAT(all_construct("aaaaaaaaaaaaaaaaaaaaaaaaa",
                             {"a", "aa", "aaa", "aaaa", "aaaaa"}),
               Not(IsEmpty()));
+}
+
+TEST(lcntstrct_tbltn, correctness) {
+  using namespace tbltn;
+  EXPECT_THAT(all_construct("hello", {"cat", "dog"}), IsEmpty());
+  EXPECT_THAT(all_construct("", {"cat", "dog"}), ElementsAre(word_bank_t{}));
+  EXPECT_THAT(all_construct("purple", {"purp", "p", "ur", "le", "purpl"}),
+              ElementsAre(word_bank_t{"purp", "le"},
+                          word_bank_t{"p", "ur", "p", "le"}));
+  EXPECT_THAT(
+      all_construct("abcdef", {"ab", "abc", "cd", "def", "abcd", "ef", "c"}),
+      UnorderedElementsAre(
+          word_bank_t{"ab", "cd", "ef"}, word_bank_t{"ab", "c", "def"},
+          word_bank_t{"abc", "def"}, word_bank_t{"abcd", "ef"}));
+  EXPECT_THAT(all_construct("skateboard",
+                            {"bo", "rd", "ate", "t", "ska", "sk", "boar"}),
+              IsEmpty());
+  EXPECT_THAT(all_construct("aaaaaaaaaaz", {"a", "aa", "aaa", "aaaa", "aaaaa"}),
+              IsEmpty());
 }
