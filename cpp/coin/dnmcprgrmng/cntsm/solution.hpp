89951ca85f3f8926e7669d1bb77b2e792fe68be5
eDyablo <eyablonsky@gmail.com>
2021-05-04 21:55:39 +0300

    count sum problem memoized solution

--- cpp/coin/dnmcprgrmng/cntsm/solution.hpp
+++ cpp/coin/dnmcprgrmng/cntsm/solution.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include <vector>
 
 namespace coin {
@@ -11,11 +12,11 @@ namespace coin {
       /**
        * m = targer sum
        * n = amount of numbers
-       * 
+       *
        * Brute force solution:
        * time:  O(2^?)
        * space: O(?)
-       */ 
+       */
       size_t count_sum(number_t target, number_bank_t const& numbers,
                        size_t taken = 0) {
         if (target == 0) return 1;
@@ -25,5 +26,36 @@ namespace coin {
                count_sum(target, numbers, taken + 1);
       }
     }  // namespace brtfrc
+
+    namespace mztn {
+      using count_sum_memo_t = std::unordered_map<size_t, size_t>;
+
+      /**
+       * m = targer sum
+       * n = amount of numbers
+       *
+       * Memoized solution:
+       * time:  O(?)
+       * space: O(m*n)
+       */
+      size_t count_sum(number_t target, number_bank_t const& numbers,
+                       size_t taken, count_sum_memo_t& memo) {
+        auto const key = target * numbers.size() + taken;
+        if (memo.find(key) != memo.end()) return memo[key];
+        if (target == 0) return 1;
+        if (target < 0) return 0;
+        if (taken >= numbers.size()) return 0;
+        auto const sum =
+            count_sum(target - numbers[taken], numbers, taken, memo) +
+            count_sum(target, numbers, taken + 1, memo);
+        memo[key] = sum;
+        return sum;
+      }
+
+      size_t count_sum(number_t target, number_bank_t const& numbers) {
+        count_sum_memo_t memo;
+        return count_sum(target, numbers, 0, memo);
+      }
+    }  // namespace mztn
   }    // namespace dnmcprgrmng
 }  // namespace coin
--- cpp/coin/dnmcprgrmng/cntsm/test.cpp
+++ cpp/coin/dnmcprgrmng/cntsm/test.cpp
@@ -19,7 +19,21 @@ TEST(cntsm_brtfrc, correctness) {
   EXPECT_THAT(count_sum(7, {7, 4, 3, 2}), Eq(3));
 }
 
-TEST(DISABLED_cntsm_brtfrc, performance) {
-  using namespace brtfrc;
-  EXPECT_THAT(count_sum(100, number_bank_t(10, 3)), Eq(0));
+TEST(cntsm_mztn, correctness) {
+  using namespace mztn;
+  EXPECT_THAT(count_sum(0, {1, 2, 3}), Eq(1));
+  EXPECT_THAT(count_sum(1, {1}), Eq(1));
+  EXPECT_THAT(count_sum(1, {2}), Eq(0));
+  EXPECT_THAT(count_sum(2, {1, 2}), Eq(2));
+  EXPECT_THAT(count_sum(3, {1, 2, 3}), Eq(3));
+  EXPECT_THAT(count_sum(6, {1, 5}), Eq(2));
+  EXPECT_THAT(count_sum(9, {5}), Eq(0));
+  EXPECT_THAT(count_sum(10, {1, 5, 10, 25}), Eq(4));
+  EXPECT_THAT(count_sum(25, {1, 5, 10, 25}), Eq(13));
+  EXPECT_THAT(count_sum(7, {7, 4, 3, 2}), Eq(3));
+}
+
+TEST(cntsm_mztn, performance) {
+  using namespace mztn;
+  EXPECT_THAT(count_sum(1000, number_bank_t(10, 3)), Eq(0));
 }
