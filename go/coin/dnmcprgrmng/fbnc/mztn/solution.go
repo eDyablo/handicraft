cadef1b970b90b6df44ffce7b471878f9195d3ac
eDyablo <eyablonsky@gmail.com>
2021-05-09 09:54:51 +0300

    refactor memoized fibonacci problem

--- go/coin/dnmcprgrmng/fbnc/mztn/solution.go
+++ go/coin/dnmcprgrmng/fbnc/mztn/solution.go
@@ -1,17 +1,22 @@
 package fbnc
 
-func Fibo(n uint) int {
-	memo := make(map[uint]int)
-	return fibo(n, memo)
+type memo struct {
+	Record map[uint]int
 }
 
-func fibo(n uint, memo map[uint]int) int {
-	if n < 3 {
-		return 1
-	}
-	if value, in := memo[n]; in {
+func (memo *memo) fibo(n uint) int {
+	if value, in := memo.Record[n]; in {
 		return value
 	}
-	memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
-	return memo[n]
+	value := int(1)
+	if n > 2 {
+		value = memo.fibo(n-1) + memo.fibo(n-2)
+	}
+	memo.Record[n] = value
+	return value
+}
+
+func fibo(n uint) int {
+	memo := &memo{make(map[uint]int)}
+	return memo.fibo(n)
 }
--- go/coin/dnmcprgrmng/fbnc/mztn/solution_test.go
+++ go/coin/dnmcprgrmng/fbnc/mztn/solution_test.go
@@ -12,7 +12,7 @@ func Test_correctness(t *testing.T) {
 		{1, 1}, {2, 1}, {3, 2}, {4, 3}, {5, 5}, {6, 8}, {7, 13}, {8, 21}, {9, 34},
 	}
 	for _, test := range tests {
-		if actual := Fibo(test.number); actual != test.expected {
+		if actual := fibo(test.number); actual != test.expected {
 			t.Error("expected fibo(", test.number, ") to be", test.expected, "but go", actual)
 		}
 	}
@@ -23,7 +23,7 @@ func Test_performance(t *testing.T) {
 		{50, 12586269025},
 	}
 	for _, test := range tests {
-		if actual := Fibo(test.number); actual != test.expected {
+		if actual := fibo(test.number); actual != test.expected {
 			t.Error("expected fibo(", test.number, ") to be", test.expected, "but go", actual)
 		}
 	}
