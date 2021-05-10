a2269516de62f8167532376a63db8784cf223411
eDyablo <eyablonsky@gmail.com>
2021-05-10 19:34:38 +0300

    remove redundant struct type

--- go/coin/dnmcprgrmng/fbnc/mztn/solution.go
+++ go/coin/dnmcprgrmng/fbnc/mztn/solution.go
@@ -1,22 +1,20 @@
 package fbnc
 
-type memo struct {
-	Record map[uint]int
-}
+type memo map[uint]int
 
-func (memo *memo) fibo(n uint) int {
-	if value, in := memo.Record[n]; in {
+func (memo memo) fibo(n uint) int {
+	if value, in := memo[n]; in {
 		return value
 	}
 	value := int(1)
 	if n > 2 {
 		value = memo.fibo(n-1) + memo.fibo(n-2)
 	}
-	memo.Record[n] = value
+	memo[n] = value
 	return value
 }
 
 func fibo(n uint) int {
-	memo := &memo{make(map[uint]int)}
+	memo := &memo{}
 	return memo.fibo(n)
 }
