973c5acd36f714cfb3f43bb48ea6a26e15cbe7ef
eDyablo <eyablonsky@gmail.com>
2021-05-15 11:26:49 +0300

    tidy up code

--- csharp/coin/dnmcprgrmng/cntsm/brtfrc/Solution.cs
+++ csharp/coin/dnmcprgrmng/cntsm/brtfrc/Solution.cs
@@ -12,7 +12,8 @@ namespace dnmcprgrmng.cntsm.brtfrc
             if (target == 0) return 1;
             if (target < 0) return 0;
             if (selected >= numbers.Length) return 0;
-            return CountSum(target - numbers[selected], numbers, selected) + CountSum(target, numbers, selected + 1);
+            return CountSum(target - numbers[selected], numbers, selected) +
+                CountSum(target, numbers, selected + 1);
         }
     }
 }
