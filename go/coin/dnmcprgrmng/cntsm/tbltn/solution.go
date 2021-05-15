0337167c2a511fbf951053411c33e91d036a3193
eDyablo <eyablonsky@gmail.com>
2021-05-15 11:57:04 +0300

    improve count sum go solution

--- go/coin/dnmcprgrmng/cntsm/tbltn/solution.go
+++ go/coin/dnmcprgrmng/cntsm/tbltn/solution.go
@@ -6,10 +6,8 @@ func count_sum(target int, numbers []int) uint {
 	table := make([]uint, uint(target)+1)
 	table[0] = 1
 	for _, number := range numbers {
-		for amount := int(1); amount <= target; amount++ {
-			if number <= amount {
-				table[amount] += table[amount-number]
-			}
+		for sub_target := number; sub_target <= target; sub_target++ {
+			table[sub_target] += table[sub_target-number]
 		}
 	}
 	return table[target]
