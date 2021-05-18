eaaa84baae2f0819fdd2f527e4763a62e6e7ab5b
eDyablo <eyablonsky@gmail.com>
2021-05-18 13:19:38 +0300

    dynamic programming fibonacci problem tabulated groovy solution

---
+++ groovy/dnmcprgrmng/src/fbnc/tbltn/Solution.groovy
@@ -0,0 +1,21 @@
+package fbnc.tbltn
+
+import groovy.transform.CompileDynamic
+
+/**
+ * Solution
+ */
+@CompileDynamic
+class Solution {
+
+  long fibo(int n) {
+    final table = [0L] * (n + 2)
+    table[1] = 1
+    for (int i = 1; i < n; ++i) {
+      table[i + 1] += table[i]
+      table[i + 2] += table[i]
+    }
+    return table[n]
+  }
+
+}
---
+++ groovy/dnmcprgrmng/test/fbnc/tbltn/SolutionTest.groovy
@@ -0,0 +1,35 @@
+package fbnc.tbltn
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.equalTo
+
+import org.junit.Test
+import groovy.transform.CompileStatic
+
+/**
+ * Tests
+ */
+@CompileStatic
+class SolutionTest {
+
+  private final Solution solution = new Solution()
+
+  @Test void correctness() {
+    assertThat(solution.fibo(0), is(equalTo(0L)))
+    assertThat(solution.fibo(1), is(equalTo(1L)))
+    assertThat(solution.fibo(2), is(equalTo(1L)))
+    assertThat(solution.fibo(3), is(equalTo(2L)))
+    assertThat(solution.fibo(4), is(equalTo(3L)))
+    assertThat(solution.fibo(5), is(equalTo(5L)))
+    assertThat(solution.fibo(6), is(equalTo(8L)))
+    assertThat(solution.fibo(7), is(equalTo(13L)))
+    assertThat(solution.fibo(8), is(equalTo(21L)))
+    assertThat(solution.fibo(9), is(equalTo(34L)))
+  }
+
+  @Test void performance() {
+    assertThat(solution.fibo(50), is(equalTo(12586269025L)))
+  }
+
+}
