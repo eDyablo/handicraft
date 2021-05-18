2ff63002dbe2bceea31251c8ef3b0d9685fde7b4
eDyablo <eyablonsky@gmail.com>
2021-05-18 13:09:17 +0300

    dynamic programming fibonacci problem memoized groovy solution

---
+++ groovy/dnmcprgrmng/src/fbnc/mztn/Solution.groovy
@@ -0,0 +1,21 @@
+package fbnc.mztn
+
+import groovy.transform.CompileDynamic
+
+/**
+ * Solution
+ */
+@CompileDynamic
+class Solution {
+
+  final memo = [:]
+
+  long fibo(int n) {
+    if (memo.containsKey(n)) { return memo[n] }
+    if (n < 1) { return 0 }
+    if (n < 3) { return 1 }
+    memo[n] = fibo(n - 1) + fibo(n - 2)
+    return memo[n]
+  }
+
+}
