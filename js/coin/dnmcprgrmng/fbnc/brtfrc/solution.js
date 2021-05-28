82df0c3dc8d7ed6a7e988bd001673a89a8c70fe7
edyablo <eyablonsky@gmail.com>
2021-05-28 21:04:55 +0300

    tidy up code

--- js/coin/dnmcprgrmng/fbnc/brtfrc/solution.js
+++ js/coin/dnmcprgrmng/fbnc/brtfrc/solution.js
@@ -1,5 +1,5 @@
 module.exports = {
-  fibonnaci: function (number) {
+  fibonnaci: (number) => {
     if (number == 0) return 0
     if (number < 3) return 1
     return this.fibonnaci(number - 1) + this.fibonnaci(number - 2)
