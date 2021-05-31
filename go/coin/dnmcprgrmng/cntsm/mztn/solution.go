81ef35bba5e561435c32ac057247925df0e6108b
eDyablo <eyablonsky@gmail.com>
2021-05-31 20:26:29 +0300

    use reference to memo instead of copy

--- go/coin/dnmcprgrmng/cntsm/mztn/solution.go
+++ go/coin/dnmcprgrmng/cntsm/mztn/solution.go
@@ -2,22 +2,22 @@ package cntsm
 
 type memo map[int]uint
 
-func (memo memo) count_sum(target int, numbers []int, selected uint) uint {
+func (memo *memo) count_sum(target int, numbers []int, selected uint) uint {
 	key := target*len(numbers) + int(selected)
-	if value, in := memo[key]; in {
+	if value, in := (*memo)[key]; in {
 		return value
 	}
 	if target == 0 {
-		memo[key] = 1
+		(*memo)[key] = 1
 	} else if target < 0 {
-		memo[key] = 0
+		(*memo)[key] = 0
 	} else if selected >= uint(len(numbers)) {
-		memo[key] = 0
+		(*memo)[key] = 0
 	} else {
-		memo[key] = memo.count_sum(target-numbers[selected], numbers, selected) +
+		(*memo)[key] = memo.count_sum(target-numbers[selected], numbers, selected) +
 			memo.count_sum(target, numbers, selected+1)
 	}
-	return memo[key]
+	return (*memo)[key]
 }
 
 func count_sum(target int, numbers []int) uint {
--- go/coin/dnmcprgrmng/fbnc/mztn/solution.go
+++ go/coin/dnmcprgrmng/fbnc/mztn/solution.go
@@ -2,15 +2,15 @@ package fbnc
 
 type memo map[uint]int
 
-func (memo memo) fibo(n uint) int {
-	if value, in := memo[n]; in {
+func (memo *memo) fibo(n uint) int {
+	if value, in := (*memo)[n]; in {
 		return value
 	}
 	value := int(1)
 	if n > 2 {
 		value = memo.fibo(n-1) + memo.fibo(n-2)
 	}
-	memo[n] = value
+	(*memo)[n] = value
 	return value
 }
 
