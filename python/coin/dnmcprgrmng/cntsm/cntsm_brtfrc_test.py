e01e05a071de3234ef6c41854d3b8825125f1a8c
eDyablo <eyablonsky@gmail.com>
2021-06-01 07:24:08 +0300

    count sum problem brute force python solution

---
+++ python/coin/dnmcprgrmng/cntsm/cntsm_brtfrc_test.py
@@ -0,0 +1,18 @@
+def count_sum(target: int, numbers: list[int], selected=0) -> int:
+    if target == 0:
+        return 1
+    if target < 0:
+        return 0
+    if selected >= len(numbers):
+        return 0
+    return count_sum(target - numbers[selected], numbers, selected) + count_sum(target, numbers, selected+1)
+
+
+def test_correctness():
+    assert count_sum(0, [1, 2, 3]) == 1
+    assert count_sum(1, [1]) == 1
+    assert count_sum(1, [2]) == 0
+    assert count_sum(2, [1, 2]) == 2
+    assert count_sum(10, [1, 5, 10, 25]) == 4
+    assert count_sum(25, [1, 5, 10, 25]) == 13
+    assert count_sum(7, [7, 4, 3, 2]) == 3
