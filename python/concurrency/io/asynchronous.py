207a0e912a0c519513d549d8b3d3ee95f5b8c4f3
eyablonskyi <eyablonskyi@pinterest.com>
2022-08-13 16:14:12 -0700

    tidy up code

--- python/concurrency/io/asynchronous.py
+++ python/concurrency/io/asynchronous.py
@@ -5,14 +5,14 @@ import aiohttp
 import time
 
 
-async def download_site(session, url):
+async def download_page(url, session):
     async with session.get(url) as response:
         print(f"Read {response.content_length} from {url}")
 
 
 async def download_all_pages(pages):
     async with aiohttp.ClientSession() as session:
-        tasks = (asyncio.ensure_future(download_site(session, url)) for url in pages)
+        tasks = (asyncio.ensure_future(download_page(url, session)) for url in pages)
         await asyncio.gather(*tasks, return_exceptions=True)
 
 
