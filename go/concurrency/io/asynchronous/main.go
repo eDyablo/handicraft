cd88cf9b3cdf46c479a46ad1dbcfc899f421dc01
eyablonskyi <eyablonskyi@pinterest.com>
2022-08-26 10:58:55 -0700

    use channel to synchronize multiple goroutines

--- go/concurrency/io/asynchronous/main.go
+++ go/concurrency/io/asynchronous/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-	"sync"
 )
 
 var pages = []string{
@@ -20,31 +19,36 @@ func get_pages() []string {
 	return result
 }
 
-func download_page(page string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	response, err := http.Get(page)
-	if err != nil {
-		panic(fmt.Sprintf("failed to get %s", page))
-	}
-	if response.StatusCode == http.StatusOK {
-		fmt.Printf("%s OK\n", page)	
-	} else {
-		fmt.Printf("%s FAIL %s\n", page, response.Status)
-	}
+func download_page(page string) chan bool {
+	result := make(chan bool)
+	go func() {
+		defer close(result)
+		response, err := http.Get(page)
+		if err != nil {
+			panic(fmt.Sprintf("failed to get %s", page))
+		}
+		if response.StatusCode == http.StatusOK {
+			fmt.Printf("%s OK\n", page)
+		} else {
+			fmt.Printf("%s FAIL %s\n", page, response.Status)
+		}
+	}()
+	return result
 }
 
 func download_pages(pages []string) {
-	var wg sync.WaitGroup
-	wg.Add(len(pages))
+	var results []chan bool
 	for _, page := range pages {
-		go download_page(page, &wg)
+		results = append(results, download_page(page))
 	}
-	wg.Wait()
+	for _, result := range results {
+		<-result
+	}
 }
 
 func main() {
 	startTime := time.Now()
 	download_pages(get_pages())
 	elapsedTime := time.Since(startTime)
-	fmt.Printf("done in %s\n", elapsedTime.Round(time.Microsecond))
+	fmt.Printf("done in %s\n", elapsedTime.Round(time.Millisecond))
 }
