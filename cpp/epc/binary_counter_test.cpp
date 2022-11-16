c6a97cd76d9643fe616f8fb2c07dcdcd4d322e34
eyablonskyi <eyablonskyi@pinterest.com>
2022-11-16 09:19:41 -0800

    tidy up binary counter tests

--- cpp/epc/binary_counter_test.cpp
+++ cpp/epc/binary_counter_test.cpp
@@ -18,44 +18,46 @@ namespace epc {
 
     TEST(BinaryCounter, ReduceAdded) {
       binary_counter_t<int, std::plus<int>> counter(0);
+      EXPECT_THAT(counter, SizeIs(0));
+
       counter.add(1);
       // [1]
-      EXPECT_THAT(counter.size(), Eq(1));
+      EXPECT_THAT(counter, SizeIs(1));
       EXPECT_THAT(counter.reduce(), Eq(1));
 
       counter.add(1);
       // [0, 2]
-      EXPECT_THAT(counter.size(), Eq(2));
+      EXPECT_THAT(counter, SizeIs(2));
       EXPECT_THAT(counter.reduce(), Eq(2));
 
       counter.add(1);
       // [1, 2]
-      EXPECT_THAT(counter.size(), Eq(2));
+      EXPECT_THAT(counter, SizeIs(2));
       EXPECT_THAT(counter.reduce(), Eq(3));
 
       counter.add(1);
       // [0, 0, 4]
-      EXPECT_THAT(counter.size(), Eq(3));
+      EXPECT_THAT(counter, SizeIs(3));
       EXPECT_THAT(counter.reduce(), Eq(4));
 
       counter.add(1);
       // [1, 0, 4]
-      EXPECT_THAT(counter.size(), Eq(3));
+      EXPECT_THAT(counter, SizeIs(3));
       EXPECT_THAT(counter.reduce(), Eq(5));
 
       counter.add(1);
       // [0, 2, 4]
-      EXPECT_THAT(counter.size(), Eq(3));
+      EXPECT_THAT(counter, SizeIs(3));
       EXPECT_THAT(counter.reduce(), Eq(6));
 
       counter.add(1);
       // [1, 2, 4]
-      EXPECT_THAT(counter.size(), Eq(3));
+      EXPECT_THAT(counter, SizeIs(3));
       EXPECT_THAT(counter.reduce(), Eq(7));
 
       counter.add(1);
       // [0, 0, 0, 8]
-      EXPECT_THAT(counter.size(), Eq(4));
+      EXPECT_THAT(counter, SizeIs(4));
       EXPECT_THAT(counter.reduce(), Eq(8));
     }
 
