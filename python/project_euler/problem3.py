d3d8f45412a85c2095cc66b5453ee4c25a853b03
eyablonskyi <eyablonskyi@pinterest.com>
2022-12-24 13:18:13 -0800

    solution for project euler problem 3

---
+++ python/project_euler/problem3.py
@@ -0,0 +1,34 @@
+"""
+Largest prime factor
+
+https://projecteuler.net/problem=3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+from functools import reduce
+from itertools import count
+from math import floor, sqrt
+
+
+def find_primes(number):
+    primes = []
+
+    for factor in range(2, floor(sqrt(number))):
+        if number % factor == 0:
+            primes.append(factor)
+            while number % factor == 0:
+                number /= factor
+
+    return primes
+
+
+number = 600851475143
+
+primes = find_primes(number)
+
+assert reduce(lambda a, b: a*b, primes) == number
+
+print(primes[-1])
